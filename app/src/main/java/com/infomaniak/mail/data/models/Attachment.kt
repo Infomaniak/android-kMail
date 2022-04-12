2b97c973e7a1eefd23a60dcb508f1ae0532d7a54
KevinBoulongne <kevin.boulongne@gmail.com>
2022-04-12 13:14:34 +0200

    Add ApiRepository, ApiRoutes, and some Models

---
+++ app/src/main/java/com/infomaniak/mail/data/models/Attachment.kt
@@ -0,0 +1,42 @@
+/*
+ * Infomaniak kMail - Android
+ * Copyright (C) 2022 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.infomaniak.mail.data.models
+
+import android.media.Image
+import java.net.URL
+
+data class Attachment(
+    val uuid: String,
+    val partId: String,
+    val mimeType: String,
+    val encoding: String,
+    val size: Int,
+    val name: String,
+    val disposition: AttachmentDisposition,
+    val contentId: String,
+    val resource: String,
+    val driveUrl: String,
+    val data: Data,
+    val localUrl: URL,
+    val thumbnail: Image,
+) {
+    enum class AttachmentDisposition {
+        INLINE,
+        ATTACHMENT,
+    }
+}
