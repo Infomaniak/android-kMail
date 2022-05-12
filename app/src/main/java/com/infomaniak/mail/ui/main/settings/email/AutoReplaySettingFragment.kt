8aefe88a2b976b9e58b430c489f0d211262a7179
KevinBoulongne <kevin.boulongne@gmail.com>
2022-05-12 11:28:46 +0200

    Format files

--- app/src/main/java/com/infomaniak/mail/ui/main/settings/email/AutoReplaySettingFragment.kt
+++ app/src/main/java/com/infomaniak/mail/ui/main/settings/email/AutoReplaySettingFragment.kt
@@ -26,9 +26,6 @@ import com.infomaniak.mail.R
 
 class AutoReplaySettingFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View = inflater.inflate(R.layout.fragment_auto_replay_settings, container, false)
-
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View =
+        inflater.inflate(R.layout.fragment_auto_replay_settings, container, false)
 }
