2b6879335d8a978011723e4cf4842c6cdb20c2a8
Fabian Devel <fabian.devel@infomaniak.com>
2022-05-25 14:18:56 +0200

    Add collapsingToolbarLayout to thread fragment

--- app/src/main/java/com/infomaniak/mail/ui/main/thread/ThreadFragment.kt
+++ app/src/main/java/com/infomaniak/mail/ui/main/thread/ThreadFragment.kt
@@ -23,6 +23,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.viewModelScope
@@ -42,7 +43,6 @@ import kotlinx.coroutines.launch
 class ThreadFragment : Fragment() {
 
     private val navigationArgs: ThreadFragmentArgs by navArgs()
-    private val mainViewModel: MainViewModel by viewModels()
     private val threadViewModel: ThreadViewModel by viewModels()
 
     private lateinit var binding: FragmentThreadBinding
@@ -62,8 +62,10 @@ class ThreadFragment : Fragment() {
     private fun setupUi() {
         with(binding) {
             messagesList.adapter = ThreadAdapter().also { threadAdapter = it }
-            backButton.setOnClickListener { findNavController().popBackStack() }
-            threadTitle.text = navigationArgs.threadSubject.displayedSubject(requireContext())
+            toolbar.setNavigationOnClickListener { findNavController().popBackStack() }
+            collapse.title = navigationArgs.threadSubject.displayedSubject(requireContext())
+            // TODO see if favorite icon must be visible or not
+            // iconFavorite.isVisible = navigationArgs.threadIsFavorite
 
             AppCompatResources.getDrawable(root.context, R.drawable.divider)?.let {
                 messagesList.addItemDecoration(DividerItemDecorator(it))
