232d63afc540d4ede9931e14d35738508e70431a
Fabian Devel <fabian.devel@infomaniak.com>
2022-06-03 16:46:26 +0200

    NewMessage fab shrink on scroll down and expand on scroll up

--- app/src/main/java/com/infomaniak/mail/ui/main/thread/ThreadListFragment.kt
+++ app/src/main/java/com/infomaniak/mail/ui/main/thread/ThreadListFragment.kt
@@ -124,12 +124,10 @@ class ThreadListFragment : Fragment(), SwipeRefreshLayout.OnRefreshListener {
         }
 
         threadsList.addOnScrollListener(object : RecyclerView.OnScrollListener() {
-            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
-                super.onScrollStateChanged(recyclerView, newState)
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
                 val layoutManager = recyclerView.layoutManager as LinearLayoutManager
-                if (layoutManager.findFirstCompletelyVisibleItemPosition() == 0 ||
-                    layoutManager.findLastCompletelyVisibleItemPosition() == threadListAdapter.itemCount - 1
-                ) {
+                if (layoutManager.findFirstCompletelyVisibleItemPosition() == 0 || dy < 0) {
                     newMessageFab.extend()
                 } else {
                     newMessageFab.shrink()
