c07348627d23121c87e3aec6bf467e73a9e4c073
KevinBoulongne <kevin.boulongne@gmail.com>
2022-07-07 10:53:03 +0200

    Add CancelDelaySetting fragment

--- app/src/main/java/com/infomaniak/mail/ui/main/settings/send/CancelDelaySettingFragment.kt
+++ app/src/main/java/com/infomaniak/mail/ui/main/settings/send/CancelDelaySettingFragment.kt
@@ -17,6 +17,64 @@
  */
 package com.infomaniak.mail.ui.main.settings.send
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.core.view.isInvisible
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.infomaniak.mail.R
+import com.infomaniak.mail.databinding.FragmentCancelDelaySettingBinding
 
-class CancelDelaySettingFragment : Fragment()
+class CancelDelaySettingFragment : Fragment() {
+
+    private lateinit var binding: FragmentCancelDelaySettingBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        return FragmentCancelDelaySettingBinding.inflate(inflater, container, false).also { binding = it }.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupBack()
+        setupUi()
+        setupListeners()
+    }
+
+    private fun setupBack() {
+        binding.toolbar.setNavigationOnClickListener { findNavController().popBackStack() }
+    }
+
+    private fun setupUi() = with(binding) {
+        val resId = R.string.settingsDelaySeconds
+        settingCancel10Text.text = getString(resId, 10)
+        settingCancel15Text.text = getString(resId, 15)
+        settingCancel20Text.text = getString(resId, 20)
+        settingCancel25Text.text = getString(resId, 25)
+        settingCancel30Text.text = getString(resId, 30)
+    }
+
+    private fun setupListeners() = with(binding) {
+        settingsDisabled.setOnClickListener { settingsDisabledCheck.selectOption() }
+        settingCancel10.setOnClickListener { settingCancel10Check.selectOption() }
+        settingCancel15.setOnClickListener { settingCancel15Check.selectOption() }
+        settingCancel20.setOnClickListener { settingCancel20Check.selectOption() }
+        settingCancel25.setOnClickListener { settingCancel25Check.selectOption() }
+        settingCancel30.setOnClickListener { settingCancel30Check.selectOption() }
+    }
+
+    private fun ImageView.selectOption() = with(binding) {
+
+        settingsDisabledCheck.let { if (it != this@selectOption) it.isInvisible = true }
+        settingCancel10Check.let { if (it != this@selectOption) it.isInvisible = true }
+        settingCancel15Check.let { if (it != this@selectOption) it.isInvisible = true }
+        settingCancel20Check.let { if (it != this@selectOption) it.isInvisible = true }
+        settingCancel25Check.let { if (it != this@selectOption) it.isInvisible = true }
+        settingCancel30Check.let { if (it != this@selectOption) it.isInvisible = true }
+
+        this@selectOption.isVisible = true
+    }
+}
