69dfe0604d38c586044ec4e0efb21697deca74cd
KevinBoulongne <kevin.boulongne@gmail.com>
2022-07-07 08:48:41 +0200

    Add ExternalContentSetting fragment

--- app/src/main/java/com/infomaniak/mail/ui/main/settings/select/ExternalContentSettingFragment.kt
+++ app/src/main/java/com/infomaniak/mail/ui/main/settings/select/ExternalContentSettingFragment.kt
@@ -17,4 +17,45 @@
  */
 package com.infomaniak.mail.ui.main.settings.select
 
-class ExternalContentSettingFragment : SelectSettingsFragment()
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.core.view.isInvisible
+import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.infomaniak.mail.databinding.FragmentExternalContentSettingBinding
+
+class ExternalContentSettingFragment : Fragment() {
+
+    private lateinit var binding: FragmentExternalContentSettingBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        return FragmentExternalContentSettingBinding.inflate(inflater, container, false).also { binding = it }.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupBack()
+        setupListeners()
+    }
+
+    private fun setupBack() {
+        binding.toolbar.setNavigationOnClickListener { findNavController().popBackStack() }
+    }
+
+    private fun setupListeners() = with(binding) {
+        settingsOptionAlways.setOnClickListener { settingsOptionAlwaysCheck.selectOption() }
+        settingsOptionAskMe.setOnClickListener { settingsOptionAskMeCheck.selectOption() }
+    }
+
+    private fun ImageView.selectOption() = with(binding) {
+
+        settingsOptionAlwaysCheck.let { if (it != this@selectOption) it.isInvisible = true }
+        settingsOptionAskMeCheck.let { if (it != this@selectOption) it.isInvisible = true }
+
+        this@selectOption.isVisible = true
+    }
+}
