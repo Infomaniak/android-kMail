453afe745ae086b380d324a9594fac59397ad2a8
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2022-07-25 16:55:50 +0200

    Detailed contacts and relative hours

---
+++ app/src/main/java/com/infomaniak/mail/ui/main/thread/DetailedRecipientAdapter.kt
@@ -0,0 +1,49 @@
+/*
+ * Infomaniak kMail - Android
+ * Copyright (C) 2022 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.infomaniak.mail.ui.main.thread
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.infomaniak.mail.data.models.Recipient
+import com.infomaniak.mail.databinding.ItemDetailedContactBinding
+
+class DetailedRecipientAdapter(
+    private val recipients: List<Recipient> = listOf()
+) : RecyclerView.Adapter<DetailedRecipientAdapter.DetailedRecipientViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DetailedRecipientViewHolder {
+        return DetailedRecipientViewHolder(ItemDetailedContactBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+    }
+
+    override fun onBindViewHolder(holder: DetailedRecipientViewHolder, position: Int): Unit = with(holder.binding) {
+        val recipient = recipients[position]
+
+        if (recipient.name.isNullOrEmpty()) {
+            name.text = recipient.email
+            emailAddress.text = ""
+        } else {
+            name.text = recipient.name
+            emailAddress.text = recipient.email
+        }
+    }
+
+    override fun getItemCount(): Int = recipients.count()
+
+    class DetailedRecipientViewHolder(val binding: ItemDetailedContactBinding) : RecyclerView.ViewHolder(binding.root)
+}
