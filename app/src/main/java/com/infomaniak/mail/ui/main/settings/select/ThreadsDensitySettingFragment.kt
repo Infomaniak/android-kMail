42fe957e745ea827490b9c63d367cdadd228870b
KevinBoulongne <kevin.boulongne@gmail.com>
2022-08-09 13:18:03 +0200

    Rename `MessageListDensity` into `ThreadsDensity`

--- app/src/main/java/com/infomaniak/mail/ui/main/settings/select/ThreadsDensitySettingFragment.kt
+++ app/src/main/java/com/infomaniak/mail/ui/main/settings/select/ThreadsDensitySettingFragment.kt
@@ -17,4 +17,4 @@
  */
 package com.infomaniak.mail.ui.main.settings.select
 
-class MessageListDensitySettingFragment : SelectSettingsFragment()
+class ThreadsDensitySettingFragment : SelectSettingsFragment()
