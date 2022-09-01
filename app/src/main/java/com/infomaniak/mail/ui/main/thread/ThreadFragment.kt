0a0bcf290105666b85c76eb5be46ebba56d55a20
KevinBoulongne <kevin.boulongne@gmail.com>
2022-09-01 13:49:08 +0200

    Replace ThreadController call in ThreadFragment with LiveData `observe`

--- app/src/main/java/com/infomaniak/mail/ui/main/thread/ThreadFragment.kt
+++ app/src/main/java/com/infomaniak/mail/ui/main/thread/ThreadFragment.kt
@@ -39,11 +39,13 @@ import com.infomaniak.mail.data.cache.mailboxContent.DraftController
 import com.infomaniak.mail.data.cache.mailboxContent.MessageController
 import com.infomaniak.mail.data.cache.mailboxContent.ThreadController
 import com.infomaniak.mail.data.models.message.Message
+import com.infomaniak.mail.data.models.thread.Thread
 import com.infomaniak.mail.databinding.FragmentThreadBinding
 import com.infomaniak.mail.ui.MainViewModel
 import com.infomaniak.mail.utils.ModelsUtils.getFormattedThreadSubject
 import com.infomaniak.mail.utils.context
 import com.infomaniak.mail.utils.notYetImplemented
+import com.infomaniak.mail.utils.observeNotNull
 import com.infomaniak.mail.utils.toSharedFlow
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -55,6 +57,7 @@ class ThreadFragment : Fragment() {
     private val navigationArgs: ThreadFragmentArgs by navArgs()
 
     private val mainViewModel: MainViewModel by viewModels()
+    private val threadViewModel: ThreadViewModel by viewModels()
 
     private lateinit var binding: FragmentThreadBinding
     private var threadAdapter = ThreadAdapter()
@@ -67,7 +70,7 @@ class ThreadFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         setupUi()
         setupAdapter()
-        listenToMessages()
+        getThread()
     }
 
     private fun setupUi() = with(binding) {
@@ -159,12 +162,14 @@ class ThreadFragment : Fragment() {
         }
     }
 
-    private fun listenToMessages() = lifecycleScope.launch(Dispatchers.IO) {
-        ThreadController.getThreadSync(navigationArgs.threadUid)?.let { thread ->
-            mainViewModel.openThread(thread)
-            thread.messages.asFlow().toSharedFlow().collect {
-                withContext(Dispatchers.Main) { displayMessages(it.list) }
-            }
+    private fun getThread() {
+        threadViewModel.getThread(navigationArgs.threadUid).observeNotNull(viewLifecycleOwner, ::listenToMessages)
+    }
+
+    private fun listenToMessages(thread: Thread) = lifecycleScope.launch(Dispatchers.IO) {
+        mainViewModel.openThread(thread)
+        thread.messages.asFlow().toSharedFlow().collect {
+            withContext(Dispatchers.Main) { displayMessages(it.list) }
         }
     }
 
@@ -174,7 +179,7 @@ class ThreadFragment : Fragment() {
         binding.messagesList.scrollToPosition(threadAdapter.lastIndex())
     }
 
-    companion object {
+    private companion object {
         const val COLLAPSE_TITLE_THRESHOLD = 0.5
     }
 }
---
+++ app/src/main/java/com/infomaniak/mail/ui/main/thread/ThreadViewModel.kt
@@ -0,0 +1,32 @@
+/*
+ * Infomaniak kMail - Android
+ * Copyright (C) 2022 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.infomaniak.mail.ui.main.thread
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import com.infomaniak.mail.data.cache.mailboxContent.ThreadController
+import com.infomaniak.mail.data.models.thread.Thread
+import kotlinx.coroutines.Dispatchers
+
+class ThreadViewModel : ViewModel() {
+
+    fun getThread(uid: String): LiveData<Thread?> = liveData(Dispatchers.IO) {
+        emit(ThreadController.getThreadSync(uid))
+    }
+}
