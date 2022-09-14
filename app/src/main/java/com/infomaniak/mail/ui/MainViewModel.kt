cbf758459fec5d8af74135c1a3102bfef458d35c
KevinBoulongne <kevin.boulongne@gmail.com>
2022-09-14 14:33:11 +0200

    Make Attachment an EmbeddedRealmObject

--- app/src/main/java/com/infomaniak/mail/ui/MainViewModel.kt
+++ app/src/main/java/com/infomaniak/mail/ui/MainViewModel.kt
@@ -256,12 +256,7 @@ class MainViewModel : ViewModel() {
                 realmMessage
             } else {
                 ApiRepository.getMessage(realmMessage.resource).data?.also { completedMessage ->
-                    completedMessage.apply {
-                        fullyDownloaded = true
-                        // TODO: Remove this `forEachIndexed` when we have EmbeddedObjects
-                        @Suppress("SAFE_CALL_WILL_CHANGE_NULLABILITY", "UNNECESSARY_SAFE_CALL")
-                        attachments?.forEachIndexed { index, attachment -> attachment.initLocalValues(index, uid) }
-                    }
+                    completedMessage.fullyDownloaded = true
                     // TODO: Uncomment this when managing Drafts folder
                     // if (completedMessage.isDraft && currentFolder.role = Folder.FolderRole.DRAFT) {
                     //     Log.e("TAG", "fetchMessagesFromApi: ${completedMessage.subject} | ${completedMessage.body?.value}")
