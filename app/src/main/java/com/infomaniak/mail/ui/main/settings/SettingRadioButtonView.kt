13f1e8e0bab95eca9fa3e07c0c95d6fd59d6a48c
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2022-09-23 15:55:43 +0200

    Radio group and radio button custom views

---
+++ app/src/main/java/com/infomaniak/mail/ui/main/settings/SettingRadioButtonView.kt
@@ -0,0 +1,82 @@
+/*
+ * Infomaniak kMail - Android
+ * Copyright (C) 2022 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.infomaniak.mail.ui.main.settings
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.FrameLayout
+import androidx.core.view.isGone
+import androidx.core.view.isVisible
+import com.infomaniak.mail.R
+import com.infomaniak.mail.databinding.ViewSettingRadioButtonBinding
+import com.infomaniak.mail.utils.getAttributeColor
+import com.google.android.material.R as RMaterial
+
+
+class SettingRadioButtonView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : FrameLayout(context, attrs, defStyleAttr), RadioCheckable {
+
+    private val binding: ViewSettingRadioButtonBinding by lazy {
+        ViewSettingRadioButtonBinding.inflate(LayoutInflater.from(context), this, true)
+    }
+
+    override val associatedValue: String?
+
+    init {
+        with(binding) {
+            if (attrs != null) {
+                val typedArray = context.obtainStyledAttributes(attrs, R.styleable.SettingRadioButtonView, 0, 0)
+
+                val iconDrawable = typedArray.getDrawable(R.styleable.SettingRadioButtonView_icon)
+                val textString = typedArray.getString(R.styleable.SettingRadioButtonView_text)
+                val checkMarkColor = typedArray.getColor(
+                    R.styleable.SettingRadioButtonView_checkMarkColor,
+                    context.getAttributeColor(RMaterial.attr.colorPrimary)
+                )
+                associatedValue = typedArray.getString(R.styleable.SettingRadioButtonView_value)
+
+                if (iconDrawable != null) icon.apply {
+                    isVisible = true
+                    setImageDrawable(iconDrawable)
+                }
+                text.text = textString
+                checkMark.setColorFilter(checkMarkColor)
+
+                root.setOnClickListener {
+                    (parent as? OnCheckListener)?.onChecked(this@SettingRadioButtonView.id)
+                }
+
+                typedArray.recycle()
+            } else {
+                associatedValue = null
+            }
+        }
+    }
+
+    override fun check() {
+        binding.checkMark.isVisible = true
+    }
+
+    override fun uncheck() {
+        binding.checkMark.isGone = true
+    }
+}
---
+++ app/src/main/java/com/infomaniak/mail/ui/main/settings/SettingRadioGroupView.kt
@@ -0,0 +1,100 @@
+/*
+ * Infomaniak kMail - Android
+ * Copyright (C) 2022 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.infomaniak.mail.ui.main.settings
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import android.widget.LinearLayout
+import androidx.annotation.IdRes
+import androidx.core.content.ContextCompat
+import androidx.core.view.children
+import com.infomaniak.mail.R
+import com.infomaniak.lib.core.R as RCore
+
+
+class SettingRadioGroupView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : LinearLayout(context, attrs, defStyleAttr), OnCheckListener {
+
+    @IdRes
+    private var checkedId: Int = View.NO_ID
+    private var checkedValue: String? = null
+    private var onItemCheckedListener: ((Int, String?) -> Unit)? = null
+    private var shouldAddDividers = true
+
+    init {
+        orientation = VERTICAL
+
+        if (attrs != null) {
+            val typedArray = context.obtainStyledAttributes(attrs, R.styleable.SettingRadioGroupView, 0, 0)
+
+            checkedId = typedArray.getResourceId(R.styleable.SettingRadioGroupView_defaultCheckedId, View.NO_ID)
+            check(checkedId)
+
+            shouldAddDividers = !typedArray.getBoolean(R.styleable.SettingRadioGroupView_ignoreDividers, false)
+
+            typedArray.recycle()
+        }
+
+        if (shouldAddDividers) {
+            showDividers = SHOW_DIVIDER_MIDDLE
+            dividerPadding = resources.getDimension(RCore.dimen.marginStandardSmall).toInt()
+            dividerDrawable = ContextCompat.getDrawable(context, R.drawable.setting_divider)
+        }
+    }
+
+    override fun onFinishInflate() {
+        children.forEach {
+            if (it is RadioCheckable && it.id == View.NO_ID) it.id = generateViewId()
+        }
+    }
+
+    override fun onChecked(@IdRes viewId: Int) {
+        if (viewId == checkedId) return
+        check(viewId)
+        onItemCheckedListener?.invoke(checkedId, checkedValue)
+    }
+
+    private fun check(@IdRes viewId: Int) {
+        if (viewId == checkedId) return
+
+        (findViewById(checkedId) as? RadioCheckable)?.uncheck()
+        val radioButton = this.findViewById(viewId) as RadioCheckable
+        radioButton.check()
+
+        checkedId = viewId
+        checkedValue = radioButton.associatedValue
+    }
+
+    fun onItemCheckedListener(listener: ((Int, String?) -> Unit)?) {
+        onItemCheckedListener = listener
+    }
+}
+
+interface RadioCheckable {
+    val associatedValue: String?
+    fun check()
+    fun uncheck()
+}
+
+interface OnCheckListener {
+    fun onChecked(@IdRes viewId: Int)
+}
