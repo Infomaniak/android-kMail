27d02ddc8f42a91add9d040d134a9adb1d4ed29b
KevinBoulongne <kevin.boulongne@gmail.com>
2022-10-24 09:27:48 +0200

    Refactor use of `UiSettings.threadMode`

--- app/src/main/java/com/infomaniak/mail/ui/main/menu/MenuDrawerFragment.kt
+++ app/src/main/java/com/infomaniak/mail/ui/main/menu/MenuDrawerFragment.kt
@@ -33,7 +33,6 @@ import com.infomaniak.lib.core.utils.UtilsUi.openUrl
 import com.infomaniak.lib.core.utils.safeNavigate
 import com.infomaniak.mail.BuildConfig
 import com.infomaniak.mail.R
-import com.infomaniak.mail.data.UiSettings
 import com.infomaniak.mail.data.models.Folder
 import com.infomaniak.mail.data.models.Folder.FolderRole
 import com.infomaniak.mail.databinding.FragmentMenuDrawerBinding
@@ -49,8 +48,6 @@ class MenuDrawerFragment : Fragment() {
     private val mainViewModel: MainViewModel by activityViewModels()
     private val menuDrawerViewModel: MenuDrawerViewModel by viewModels()
 
-    private val uiSettings by lazy { UiSettings.getInstance(requireContext()) }
-
     var exitDrawer: (() -> Unit)? = null
     var isDrawerOpen: (() -> Boolean)? = null
 
@@ -59,7 +56,7 @@ class MenuDrawerFragment : Fragment() {
     private var canNavigate = true
 
     private val addressAdapter = MenuDrawerSwitchUserMailboxesAdapter { selectedMailbox ->
-        mainViewModel.openMailbox(selectedMailbox, uiSettings.threadMode)
+        mainViewModel.openMailbox(selectedMailbox)
         closeDrawer()
     }
 
@@ -245,7 +242,7 @@ class MenuDrawerFragment : Fragment() {
     }
 
     private fun openFolder(folderId: String) {
-        mainViewModel.openFolder(folderId, uiSettings.threadMode)
+        mainViewModel.openFolder(folderId)
         closeDrawer()
     }
 
