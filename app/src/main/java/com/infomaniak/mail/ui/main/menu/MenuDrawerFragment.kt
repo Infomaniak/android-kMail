875e1d2d1568a9e604d0491b0c88fae967db9bd4
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2022-11-11 15:58:18 +0100

    Removed get more storage bottom sheet

--- app/src/main/java/com/infomaniak/mail/ui/main/menu/MenuDrawerFragment.kt
+++ app/src/main/java/com/infomaniak/mail/ui/main/menu/MenuDrawerFragment.kt
@@ -159,7 +159,6 @@ class MenuDrawerFragment : Fragment() {
             // TODO: Restore mails
             notYetImplemented()
         }
-        getMoreStorageCardview.setOnClickListener { menuDrawerSafeNavigate(R.id.getMoreStorageBottomSheetDialog) }
     }
 
     private fun menuDrawerSafeNavigate(destinationResId: Int) {
@@ -217,7 +216,7 @@ class MenuDrawerFragment : Fragment() {
         menuDrawerViewModel.quotas.observe(viewLifecycleOwner) { quotas ->
             val isLimited = quotas != null
 
-            getMoreStorageCardview.isVisible = isLimited
+            storageLayout.isVisible = isLimited
             storageDivider.isVisible = isLimited
 
             if (isLimited) {
