f9afc2bfbb4f299a7570697019ae917cce7d4946
Abdourahamane Boinaidi <abdourahamane.boinaidi@infomaniak.com>
2022-11-24 09:53:51 +0100

    Improve draft actions work from background

--- app/src/main/java/com/infomaniak/mail/workers/DraftsActionsWorker.kt
+++ app/src/main/java/com/infomaniak/mail/workers/DraftsActionsWorker.kt
@@ -38,10 +38,17 @@ class DraftsActionsWorker(appContext: Context, params: WorkerParameters) : Corou
     private val mailboxContentRealm by lazy { RealmDatabase.newMailboxContentInstance }
     private val mailboxInfoRealm by lazy { RealmDatabase.newMailboxInfoInstance }
 
+    private lateinit var mailboxObjectId: String
+
     override suspend fun doWork(): Result = withContext(Dispatchers.IO) {
         if (runAttemptCount > MAX_RETRIES) return@withContext Result.failure()
         runCatching {
+
+            if (AccountUtils.currentUser == null) AccountUtils.requestCurrentUser() ?: return@runCatching Result.failure()
             if (AccountUtils.currentMailboxId == AppSettings.DEFAULT_ID) return@runCatching Result.failure()
+
+            mailboxObjectId = inputData.getString(MAILBOX_OBJECT_ID_KEY) ?: return@runCatching Result.failure()
+
             handleDraftsActions()
         }.getOrElse { exception ->
             exception.printStackTrace()
@@ -62,14 +69,14 @@ class DraftsActionsWorker(appContext: Context, params: WorkerParameters) : Corou
         return mailboxContentRealm.writeBlocking {
 
             fun getCurrentMailboxUuid(): String? {
-                val mailboxObjectId = MainViewModel.currentMailboxObjectId.value
-                return mailboxObjectId?.let { MailboxController.getMailbox(it, mailboxInfoRealm) }?.uuid
+                return MailboxController.getMailbox(mailboxObjectId, mailboxInfoRealm)?.uuid
             }
 
             val drafts = DraftController.getDraftsWithActions(realm = this).ifEmpty { null }
                 ?: return@writeBlocking Result.failure()
             val mailboxUuid = getCurrentMailboxUuid() ?: return@writeBlocking Result.failure()
             var hasRemoteException = false
+
 
             drafts.reversed().forEach { draft ->
                 try {
@@ -91,33 +98,32 @@ class DraftsActionsWorker(appContext: Context, params: WorkerParameters) : Corou
         private const val WORK_NAME = "SaveDraftWithAttachments"
         private const val MAX_RETRIES = 3
 
+        private const val MAILBOX_OBJECT_ID_KEY = "mailboxObjectIdKey"
+
         fun scheduleWork(context: Context, localDraftUuid: String? = null) {
 
             if (AccountUtils.currentMailboxId == AppSettings.DEFAULT_ID) return
             if (DraftController.getDraftsWithActionsCount() == 0L) return
 
-            val workRequest = OneTimeWorkRequestBuilder<DraftsActionsWorker>()
+            val currentMailboxObjectId = MainViewModel.currentMailboxObjectId.value ?: return
+            val uploadAttachmentsWorkRequest = UploadAttachmentsWorker.getWorkRequest(localDraftUuid) ?: return
+            val draftActionsWorkRequest = OneTimeWorkRequestBuilder<DraftsActionsWorker>()
                 .addTag(TAG)
+                .setInputData(workDataOf(MAILBOX_OBJECT_ID_KEY to currentMailboxObjectId))
                 .setConstraints(Constraints.Builder().setRequiredNetworkType(NetworkType.CONNECTED).build())
                 .setExpeditedWorkRequest()
                 .build()
 
-            val workManager = WorkManager.getInstance(context)
-
-            if (localDraftUuid == null) {
-                // Begin unique work, only start DraftActionsWorker
-                workManager.enqueueUniqueWork(TAG, ExistingWorkPolicy.REPLACE, workRequest)
-            } else {
-                // Begin unique chain work, upload attachments before save current draft
-                val uploadAttachmentsWorkRequest = UploadAttachmentsWorker.getWorkRequest(localDraftUuid) ?: return
-                workManager.beginUniqueWork(WORK_NAME, ExistingWorkPolicy.APPEND_OR_REPLACE, uploadAttachmentsWorkRequest)
-                    .then(workRequest)
-                    .enqueue()
-            }
+            WorkManager.getInstance(context)
+                .beginUniqueWork(WORK_NAME, ExistingWorkPolicy.APPEND_OR_REPLACE, uploadAttachmentsWorkRequest)
+                .then(draftActionsWorkRequest)
+                .enqueue()
         }
 
         fun getRunningWorkInfosLiveData(context: Context): LiveData<MutableList<WorkInfo>> {
-            val workQuery = WorkQuery.Builder.fromTags(listOf(TAG)).addStates(listOf(WorkInfo.State.RUNNING)).build()
+            val workQuery = WorkQuery.Builder.fromTags(listOf(TAG))
+                .addStates(listOf(WorkInfo.State.ENQUEUED, WorkInfo.State.BLOCKED, WorkInfo.State.RUNNING))
+                .build()
             return WorkManager.getInstance(context).getWorkInfosLiveData(workQuery)
         }
     }
