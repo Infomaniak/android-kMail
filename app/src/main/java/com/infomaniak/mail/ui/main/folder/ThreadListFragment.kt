e725fb1902b85885066cbed644ccd3395d72f86d
KevinBoulongne <kevin.boulongne@gmail.com>
2023-01-20 09:39:59 +0100

    fix(ThreadListFragment): Fix wrong liveData usage

--- app/src/main/java/com/infomaniak/mail/ui/main/folder/ThreadListFragment.kt
+++ app/src/main/java/com/infomaniak/mail/ui/main/folder/ThreadListFragment.kt
@@ -434,7 +434,7 @@ class ThreadListFragment : Fragment(), SwipeRefreshLayout.OnRefreshListener {
     private fun handleThreadsVisibility(threadsCount: Int) {
         Log.d("UI", "Received threads (${threadsCount})")
 
-        val cursor = mainViewModel.currentFolderLive.value?.cursor
+        val cursor = mainViewModel.currentFolder.value?.cursor
 
         if (cursor != null && threadsCount == 0) {
             displayNoEmailView()
