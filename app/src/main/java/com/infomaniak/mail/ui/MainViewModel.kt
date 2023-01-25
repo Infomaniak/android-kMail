a465e35fb00a37c4da428ae60979cc7a98503538
KevinBoulongne <kevin.boulongne@gmail.com>
2023-01-25 16:37:17 +0100

    refactor(PR): Apply suggestions from code review

--- app/src/main/java/com/infomaniak/mail/ui/MainViewModel.kt
+++ app/src/main/java/com/infomaniak/mail/ui/MainViewModel.kt
@@ -491,20 +491,18 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
     }
     //endregion
 
-    fun undoAction(undoData: UndoData) {
-        viewModelScope.launch(Dispatchers.IO) {
-            val mailbox = currentMailbox.value ?: return@launch
-            val (resource, foldersIds, destinationFolderId) = undoData
+    fun undoAction(undoData: UndoData) = viewModelScope.launch(Dispatchers.IO) {
+        val mailbox = currentMailbox.value ?: return@launch
+        val (resource, foldersIds, destinationFolderId) = undoData
 
-            val snackbarTitle = if (ApiRepository.undoAction(resource).data == true) {
-                refreshFolders(mailbox, foldersIds, destinationFolderId)
-                R.string.snackbarMoveCancelled
-            } else {
-                RCore.string.anErrorHasOccurred
-            }
-
-            snackbarFeedback.postValue(context.getString(snackbarTitle) to null)
+        val snackbarTitle = if (ApiRepository.undoAction(resource).data == true) {
+            refreshFolders(mailbox, foldersIds, destinationFolderId)
+            R.string.snackbarMoveCancelled
+        } else {
+            RCore.string.anErrorHasOccurred
         }
+
+        snackbarFeedback.postValue(context.getString(snackbarTitle) to null)
     }
 
     private fun getMenuFolders(folders: List<Folder>): Triple<Folder?, List<Folder>, List<Folder>> {
