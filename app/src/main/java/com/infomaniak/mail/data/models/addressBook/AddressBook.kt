0981fd2f5d1cc5663282ecc7ea8e0532f43c2a19
KevinBoulongne <kevin.boulongne@gmail.com>
2023-01-25 13:02:49 +0100

    fix(AddressBook): Add `description` nullability

--- app/src/main/java/com/infomaniak/mail/data/models/addressBook/AddressBook.kt
+++ app/src/main/java/com/infomaniak/mail/data/models/addressBook/AddressBook.kt
@@ -1,6 +1,6 @@
 /*
  * Infomaniak kMail - Android
- * Copyright (C) 2022 Infomaniak Network SA
+ * Copyright (C) 2022-2023 Infomaniak Network SA
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -35,7 +35,7 @@ class AddressBook : RealmObject {
     var name: String = ""
     @SerialName("principal_uri")
     var principalUri: String = ""
-    var description: String = ""
+    var description: String? = null
     var color: String = ""
     @SerialName("is_activated")
     var isActivated: Boolean = false
