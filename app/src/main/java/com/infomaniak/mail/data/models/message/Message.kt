19481930a9a1aaf78fcda30cb58d5344ed9fae2b
Fabian Devel <fabian.devel@infomaniak.com>
2023-02-15 12:10:51 +0100

    fix(AppCrash): Add sentry log and fix app crash when messages are duplicated in realm

--- app/src/main/java/com/infomaniak/mail/data/models/message/Message.kt
+++ app/src/main/java/com/infomaniak/mail/data/models/message/Message.kt
@@ -23,7 +23,6 @@ import com.infomaniak.lib.core.utils.Utils.enumValueOfOrNull
 import com.infomaniak.mail.data.api.RealmInstantSerializer
 import com.infomaniak.mail.data.api.RealmListSerializer
 import com.infomaniak.mail.data.cache.mailboxContent.FolderController
-import com.infomaniak.mail.data.cache.mailboxContent.FolderController.SEARCH_FOLDER_ID
 import com.infomaniak.mail.data.models.Attachment
 import com.infomaniak.mail.data.models.Folder
 import com.infomaniak.mail.data.models.Folder.FolderRole
@@ -31,6 +30,7 @@ import com.infomaniak.mail.data.models.correspondent.Recipient
 import com.infomaniak.mail.data.models.draft.Priority
 import com.infomaniak.mail.data.models.getMessages.GetMessagesUidsDeltaResult.MessageFlags
 import com.infomaniak.mail.data.models.thread.Thread
+import com.infomaniak.mail.utils.AccountUtils
 import com.infomaniak.mail.utils.toRealmInstant
 import com.infomaniak.mail.utils.toShortUid
 import io.realm.kotlin.TypedRealm
@@ -38,6 +38,8 @@ import io.realm.kotlin.ext.*
 import io.realm.kotlin.types.*
 import io.realm.kotlin.types.annotations.Ignore
 import io.realm.kotlin.types.annotations.PrimaryKey
+import io.sentry.Sentry
+import io.sentry.SentryLevel
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.Transient
@@ -130,7 +132,21 @@ class Message : RealmObject {
     val threadsDuplicatedIn by backlinks(Thread::duplicates)
 
     private val _folders by backlinks(Folder::messages)
-    val folder get() = _folders.single { _folders.count() == 1 || it.id != SEARCH_FOLDER_ID }
+    // val folder get() = _folders.single { _folders.count() == 1 || it.id != SEARCH_FOLDER_ID }
+    val folder
+        get() = run {
+            if (_folders.count() > 1) {
+                Sentry.withScope { scope ->
+                    scope.level = SentryLevel.ERROR
+                    scope.setExtra("folders", "${_folders.map { it.name }}")
+                    scope.setExtra("messageUid", uid)
+                    scope.setExtra("email", AccountUtils.currentMailboxEmail.toString())
+                    Sentry.captureMessage("Message has several parent folders, it should not be possible")
+                }
+            }
+            // TODO: Replace with `.single()` when the crash is fixed
+            _folders.first()
+        }
 
     inline val shortUid get() = uid.toShortUid()
     inline val sender get() = from.first()
