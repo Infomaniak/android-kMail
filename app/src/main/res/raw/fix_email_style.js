ac646abe6ef81ae8229a929f9406f8d231308229
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2023-05-11 15:21:31 +0200

    Remove cognitive complexity of `removeAllProperties()`

--- app/src/main/res/raw/fix_email_style.js
+++ app/src/main/res/raw/fix_email_style.js
@@ -20,40 +20,56 @@
     const properties = [
         { name: 'position', values: ['absolute'] },
         // { name: '-webkit-text-size-adjust', values: [] } // For iOS only, because they're based on webkit
+        // { name: 'height', values: ['100%'] }
     ];
     removeCSSProperty(properties)
 }
 
 function removeCSSProperty(properties) {
-    // Remove properties from inline styles
+    removeFromInlineStyle(properties);
+    removeFromStylesheets(properties);
+}
+
+function removeFromInlineStyle(properties) {
     for (const property of properties) {
         const elementsWithInlineStyle = document.querySelectorAll(`[style*=${property.name}]`);
         for (const element of elementsWithInlineStyle) {
-            if (property.values.length === 0 || property.values.includes(element.style[property.name].toLowerCase().trim())) {
+            const propertyValue = element.style[property.name];
+            if (shouldRemovePropertyForGivenValue(property, propertyValue)) {
                 element.style[property.name] = null;
                 console.info(`[FIX_EMAIL_STYLE] Remove property ${property.name} from inline style.`);
             }
         }
     }
+}
 
-    // Remove properties from style tag
+function removeFromStylesheets(properties) {
     for (let i = 0; i < document.styleSheets.length; i++) {
         const styleSheet = document.styleSheets[i];
         try {
-            for (let j = 0; j < styleSheet.cssRules.length; j++) {
-                for (const property of properties) {
-                        if (!styleSheet.cssRules[j].style) { continue; }
-
-                        if (property.values.length === 0 || property.values.includes(styleSheet.cssRules[j].style[property.name].toLowerCase().trim())) {
-                            const removedValue = styleSheet.cssRules[j].style?.removeProperty(property.name);
-                            if (removedValue) {
-                                console.info(`[FIX_EMAIL_STYLE] Remove property ${property.name} from style tag.`);
-                            }
-                        }
-                }
-            }
+            removePropertiesForAllCSSRules(properties, styleSheet);
         } catch (error) {
             // The stylesheet cannot be modified
         }
     }
+}
+
+function removePropertiesForAllCSSRules(properties, styleSheet) {
+    for (let j = 0; j < styleSheet.cssRules.length; j++) {
+        for (const property of properties) {
+            if (!styleSheet.cssRules[j].style) { continue; }
+
+            const propertyValue = styleSheet.cssRules[j].style[property.name];
+            if (shouldRemovePropertyForGivenValue(property, propertyValue)) {
+                const removedValue = styleSheet.cssRules[j].style?.removeProperty(property.name);
+                if (removedValue) {
+                    console.info(`[FIX_EMAIL_STYLE] Remove property ${property.name} from style tag.`);
+                }
+            }
+        }
+    }
+}
+
+function shouldRemovePropertyForGivenValue(property, value) {
+    return property.values.length === 0 || property.values.includes(value.toLowerCase().trim())
 }
