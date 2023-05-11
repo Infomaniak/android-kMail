dd63c022b8ac4dfe8e0a926a2a30de58cfdb02d4
Abdourahamane Boinaidi <abdourahamane.boinaidi@infomaniak.com>
2023-05-11 11:10:00 +0200

    fix: FirebaseMessaging.getInstance not exists in fdroid flavor

--- app/src/main/java/com/infomaniak/mail/workers/SyncMailboxesWorker.kt
+++ app/src/main/java/com/infomaniak/mail/workers/SyncMailboxesWorker.kt
@@ -22,9 +22,9 @@ import android.util.Log
 import androidx.hilt.work.HiltWorker
 import androidx.work.*
 import androidx.work.PeriodicWorkRequest.Companion.MIN_PERIODIC_INTERVAL_MILLIS
+import com.infomaniak.mail.GplayUtils.isGooglePlayServicesNotAvailable
 import com.infomaniak.mail.data.cache.RealmDatabase
 import com.infomaniak.mail.data.cache.mailboxInfo.MailboxController
-import com.infomaniak.mail.isGooglePlayServicesNotAvailable
 import com.infomaniak.mail.utils.AccountUtils
 import com.infomaniak.mail.utils.FetchMessagesManager
 import kotlinx.coroutines.Dispatchers
