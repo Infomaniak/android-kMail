41d4484e75e642085f937792800bc2adddf02adf
Kevin Boulongne <kevin.boulongne@infomaniak.com>
2023-06-07 11:21:11 +0200

    Revert "fix(ThreadListFragment): Remove autoscroll to top when a new Thread is received"
    This reverts commit 6d4c1911d96ed319987097fdeafb58a1c223b0c1.

--- app/src/main/java/com/infomaniak/mail/ui/main/folder/ThreadListFragment.kt
+++ app/src/main/java/com/infomaniak/mail/ui/main/folder/ThreadListFragment.kt
@@ -90,6 +90,7 @@ class ThreadListFragment : Fragment(), SwipeRefreshLayout.OnRefreshListener {
 
     private lateinit var threadListAdapter: ThreadListAdapter
     private var lastUpdatedDate: Date? = null
+    private var previousFirstThreadUid: String? = null
 
     @Inject
     lateinit var draftsActionsWorkerScheduler: DraftsActionsWorker.Scheduler
@@ -408,6 +409,8 @@ class ThreadListFragment : Fragment(), SwipeRefreshLayout.OnRefreshListener {
             }
             waitingBeforeNotifyAdapter = isRecoveringFinished
             afterUpdateAdapter = { threads ->
+                if (firstThreadHasChanged(threads)) scrollToTop()
+
                 if (mainViewModel.currentFilter.value == ThreadFilter.UNSEEN && threads.isEmpty()) {
                     mainViewModel.currentFilter.value = ThreadFilter.ALL
                 }
@@ -451,8 +454,6 @@ class ThreadListFragment : Fragment(), SwipeRefreshLayout.OnRefreshListener {
                 updateFolderRole(folder.role)
                 updateLoadMore(shouldDisplayLoadMore = false)
             }
-
-            scrollToTop()
         }
     }
 
@@ -546,6 +547,13 @@ class ThreadListFragment : Fragment(), SwipeRefreshLayout.OnRefreshListener {
             title = getString(emptyState.titleId)
             description = getString(emptyState.descriptionId)
             isVisible = true
+        }
+    }
+
+    private fun firstThreadHasChanged(threads: List<Thread>): Boolean {
+        val firstThreadCustomUid = "${threads.firstOrNull()?.uid}_${AccountUtils.currentMailboxId}"
+        return (firstThreadCustomUid != previousFirstThreadUid).also {
+            previousFirstThreadUid = firstThreadCustomUid
         }
     }
 
