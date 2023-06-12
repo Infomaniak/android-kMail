220fb240469a4de5623ecde6c6044a737d064dfd
Fabian Devel <fabian.devel@infomaniak.com>
2023-06-12 14:16:35 +0200

    fix(Mailto): Avoid 422 validation failed because of invalid recipients in mailto

--- app/src/main/java/com/infomaniak/mail/ui/main/newMessage/NewMessageFragment.kt
+++ app/src/main/java/com/infomaniak/mail/ui/main/newMessage/NewMessageFragment.kt
@@ -378,7 +378,7 @@ class NewMessageFragment : Fragment() {
     private fun handleMailTo() = with(newMessageViewModel) {
         fun String.splitToRecipientList() = split(",").mapNotNull {
             val email = it.trim()
-            if (it.isBlank()) null else Recipient().initLocalValues(email, email)
+            if (email.isNotBlank() && email.isEmail()) Recipient().initLocalValues(email, email) else null
         }
 
         val intent = requireActivity().intent
