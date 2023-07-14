1f4e3be2dee4091c21915839541a35c1243fb767
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2023-07-14 16:41:56 +0200

    Creating fragment for new mail view

--- app/src/main/java/com/infomaniak/mail/ui/login/NewAccountFragment.kt
+++ app/src/main/java/com/infomaniak/mail/ui/login/NewAccountFragment.kt
@@ -17,23 +17,193 @@
  */
 package com.infomaniak.mail.ui.login
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.StringRes
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import com.infomaniak.lib.core.InfomaniakCore
+import com.infomaniak.lib.core.R
+import com.infomaniak.lib.core.models.ApiResponse
+import com.infomaniak.lib.core.models.user.User
+import com.infomaniak.lib.core.networking.HttpClient
+import com.infomaniak.lib.core.utils.SnackbarUtils.showSnackbar
+import com.infomaniak.lib.core.utils.context
+import com.infomaniak.lib.login.ApiToken
+import com.infomaniak.lib.login.InfomaniakLogin
+import com.infomaniak.mail.BuildConfig
+import com.infomaniak.mail.MatomoMail.trackAccountEvent
+import com.infomaniak.mail.MatomoMail.trackUserInfo
+import com.infomaniak.mail.data.api.ApiRepository
+import com.infomaniak.mail.data.cache.mailboxInfo.MailboxController
+import com.infomaniak.mail.databinding.FragmentNewAccountBinding
+import com.infomaniak.mail.di.IoDispatcher
+import com.infomaniak.mail.di.MainDispatcher
+import com.infomaniak.mail.utils.AccountUtils
+import com.infomaniak.mail.utils.Utils
+import com.infomaniak.mail.utils.getInfomaniakLogin
+import com.infomaniak.mail.utils.launchNoValidMailboxesActivity
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class NewAccountFragment : Fragment() {
 
     private lateinit var binding: FragmentNewAccountBinding
+
+    private lateinit var infomaniakLogin: InfomaniakLogin
+
+    @Inject
+    @IoDispatcher
+    lateinit var ioDispatcher: CoroutineDispatcher
+
+    @Inject
+    @MainDispatcher
+    lateinit var mainDispatcher: CoroutineDispatcher
+
+    private val webViewLoginResultLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            with(result) {
+                if (resultCode == AppCompatActivity.RESULT_OK) {
+                    val authCode = data?.getStringExtra(InfomaniakLogin.CODE_TAG)
+                    val translatedError = data?.getStringExtra(InfomaniakLogin.ERROR_TRANSLATED_TAG)
+                    when {
+                        translatedError?.isNotBlank() == true -> showError(translatedError)
+                        authCode?.isNotBlank() == true -> authenticateUser(authCode)
+                        else -> showError(getString(R.string.anErrorHasOccurred))
+                    }
+                } else {
+                    enableConnectButtons()
+                }
+            }
+        }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         return FragmentNewAccountBinding.inflate(inflater, container, false).also { binding = it }.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) = with(binding) {
         super.onViewCreated(view, savedInstanceState)
 
+        infomaniakLogin = context.getInfomaniakLogin()
 
+        createNewAddressButton.setOnClickListener {
+            createNewAddressButton.isEnabled = false
+            requireContext().trackAccountEvent("openCreationWebview")
+            infomaniakLogin.startCreateAccountWebView(
+                resultLauncher = createAccountResultLauncher,
+                createAccountUrl = BuildConfig.CREATE_ACCOUNT_URL,
+                successHost = BuildConfig.CREATE_ACCOUNT_SUCCESS_HOST,
+                cancelHost = BuildConfig.CREATE_ACCOUNT_CANCEL_HOST,
+            )
+        }
+    }
+
+    private val createAccountResultLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            result.handleCreateAccountActivityResult()
+        }
+
+    private fun ActivityResult.handleCreateAccountActivityResult() {
+        if (resultCode == AppCompatActivity.RESULT_OK) {
+            val translatedError = data?.getStringExtra(InfomaniakLogin.ERROR_TRANSLATED_TAG)
+            when {
+                translatedError.isNullOrBlank() -> infomaniakLogin.startWebViewLogin(webViewLoginResultLauncher, false)
+                else -> showError(translatedError)
+            }
+        }
+        enableConnectButtons()
+    }
+
+    private fun showError(error: String) {
+        showSnackbar(error)
+        enableConnectButtons()
+    }
+
+    private fun enableConnectButtons() {
+        binding.createNewAddressButton.isEnabled = true
+    }
+
+    private fun authenticateUser(authCode: String) = lifecycleScope.launch(ioDispatcher) {
+        infomaniakLogin.getToken(
+            okHttpClient = HttpClient.okHttpClientNoTokenInterceptor,
+            code = authCode,
+            onSuccess = ::onAuthenticateUserSuccess,
+            onError = ::onAuthenticateUserError,
+        )
+    }
+
+    private fun onAuthenticateUserSuccess(apiToken: ApiToken) = lifecycleScope.launch(ioDispatcher) {
+        when (val returnValue = authenticateUser(requireContext(), apiToken)) {
+            is User -> {
+                requireContext().trackAccountEvent("loggedIn")
+                AccountUtils.reloadApp?.invoke()
+            }
+            is Utils.MailboxErrorCode -> withContext(mainDispatcher) {
+                when (returnValue) {
+                    Utils.MailboxErrorCode.NO_MAILBOX -> launchNoMailboxActivity()
+                    Utils.MailboxErrorCode.NO_VALID_MAILBOX -> launchNoValidMailboxesActivity()
+                }
+            }
+            is ApiResponse<*> -> withContext(mainDispatcher) { showError(getString(returnValue.translatedError)) }
+            else -> withContext(mainDispatcher) { showError(getString(R.string.anErrorHasOccurred)) }
+        }
+    }
+
+    private fun onAuthenticateUserError(errorStatus: InfomaniakLogin.ErrorStatus) {
+        val errorResId = when (errorStatus) {
+            InfomaniakLogin.ErrorStatus.SERVER -> R.string.serverError
+            InfomaniakLogin.ErrorStatus.CONNECTION -> R.string.connectionError
+            else -> R.string.anErrorHasOccurred
+        }
+        showError(getString(errorResId))
+    }
+
+    companion object {
+        suspend fun authenticateUser(context: Context, apiToken: ApiToken): Any {
+            if (AccountUtils.getUserById(apiToken.userId) != null) return getErrorResponse(R.string.errorUserAlreadyPresent)
+
+            InfomaniakCore.bearerToken = apiToken.accessToken
+            val userProfileResponse = ApiRepository.getUserProfile(HttpClient.okHttpClientNoTokenInterceptor)
+
+            if (userProfileResponse.result == ApiResponse.Status.ERROR) return userProfileResponse
+            if (userProfileResponse.data == null) return getErrorResponse(R.string.anErrorHasOccurred)
+
+            val user = userProfileResponse.data!!.apply {
+                this.apiToken = apiToken
+                this.organizations = arrayListOf()
+            }
+
+            val apiResponse = ApiRepository.getMailboxes(HttpClient.okHttpClientNoTokenInterceptor)
+
+            return when {
+                !apiResponse.isSuccess() -> apiResponse
+                apiResponse.data?.isEmpty() == true -> Utils.MailboxErrorCode.NO_MAILBOX
+                else -> {
+                    apiResponse.data?.let { mailboxes ->
+                        context.trackUserInfo("nbMailboxes", mailboxes.count())
+                        AccountUtils.addUser(user)
+                        MailboxController.updateMailboxes(context, mailboxes)
+
+                        return@let if (mailboxes.none { it.isValid }) Utils.MailboxErrorCode.NO_VALID_MAILBOX else user
+                    } ?: run {
+                        getErrorResponse(R.string.serverError)
+                    }
+                }
+            }
+        }
+
+        private fun getErrorResponse(@StringRes text: Int): ApiResponse<Any> {
+            return ApiResponse(result = ApiResponse.Status.ERROR, translatedError = text)
+        }
     }
 }
