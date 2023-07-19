22ced7679f7c90cf444ed84547ab31110c37778d
Fabian Devel <fabian.devel@infomaniak.com>
2023-07-19 14:09:04 +0200

    refactor(MenuDrawerItemView): Create Folders ItemView

---
+++ app/src/main/java/com/infomaniak/mail/views/FolderItemView.kt
@@ -0,0 +1,39 @@
+/*
+ * Infomaniak ikMail - Android
+ * Copyright (C) 2023 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.infomaniak.mail.views
+
+import android.content.Context
+import android.util.AttributeSet
+import com.infomaniak.lib.core.utils.getAttributes
+import com.infomaniak.lib.core.utils.setMarginsRelative
+import com.infomaniak.mail.R
+import com.infomaniak.mail.databinding.ViewDecoratedTextItemBinding
+
+interface FolderItemView {
+
+    val binding: ViewDecoratedTextItemBinding
+    var indent: Int
+
+    fun setIndent() = binding.itemName.setMarginsRelative(start = indent)
+
+    fun AttributeSet.getIndent(context: Context) {
+        getAttributes(context, R.styleable.DecoratedTextItemView) {
+            indent = getDimensionPixelSize(R.styleable.DecoratedTextItemView_indent, indent)
+        }
+    }
+}
---
+++ app/src/main/java/com/infomaniak/mail/views/MoveFolderItemView.kt
@@ -0,0 +1,38 @@
+/*
+ * Infomaniak ikMail - Android
+ * Copyright (C) 2023 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.infomaniak.mail.views
+
+import android.content.Context
+import android.util.AttributeSet
+
+class MoveFolderItemView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0,
+) : SelectableTextItemView(context, attrs, defStyleAttr), FolderItemView {
+
+    override var indent = 0
+        set(value) {
+            field = value
+            setIndent()
+        }
+
+    init {
+        attrs?.getIndent(context)
+    }
+}
