9e790322adf7bd030e417f234b63fd521b283594
Abdourahamane Boinaidi <abdourahamane.boinaidi@infomaniak.com>
2023-08-17 09:33:45 +0200

    feat: (Hilt) - Add a StandardEntryPoint

---
+++ app/src/standard/java/com/infomaniak/mail/di/StandardEntryPoint.kt
@@ -0,0 +1,33 @@
+/*
+ * Infomaniak ikMail - Android
+ * Copyright (C) 2023 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.infomaniak.mail.di
+
+import androidx.work.WorkManager
+import com.infomaniak.mail.firebase.RegisterUserDeviceWorker
+import dagger.hilt.EntryPoint
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@EntryPoint
+@InstallIn(SingletonComponent::class)
+interface StandardEntryPoint {
+
+    fun registerUserDeviceWorkerScheduler(): RegisterUserDeviceWorker.Scheduler
+
+    fun workManager(): WorkManager
+}
