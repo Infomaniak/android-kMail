98f1e395006788c0b4c639d41246dca35aebfb04
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2023-09-18 09:07:07 +0200

    Restore balance to the force

--- app/src/main/java/com/infomaniak/mail/data/api/ApiRoutes.kt
+++ app/src/main/java/com/infomaniak/mail/data/api/ApiRoutes.kt
@@ -40,8 +40,7 @@ object ApiRoutes {
         return "$MAIL_API/api/mailbox/permissions?user_mailbox_id=$linkId&product_id=$hostingId"
     }
 
-    // TODO: Waiting for final route
-    fun ai(): String = "$MAIL_API/api/ia"
+    fun ai(): String = "$MAIL_API/api/ai"
 
     private fun apiMailbox(mailboxHostingId: Int, mailboxName: String): String {
         return "$INFOMANIAK_API_V1/mail_hostings/$mailboxHostingId/mailboxes/$mailboxName"
