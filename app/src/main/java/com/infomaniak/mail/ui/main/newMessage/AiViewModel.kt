e8ffa55404a36607118469e7227fe1aaf5c8a57f
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2023-09-20 11:00:48 +0200

    Add TODO to not forget to detect RATE_LIMIT_EXCEEDED

--- app/src/main/java/com/infomaniak/mail/ui/main/newMessage/AiViewModel.kt
+++ app/src/main/java/com/infomaniak/mail/ui/main/newMessage/AiViewModel.kt
@@ -46,6 +46,7 @@ class AiViewModel @Inject constructor(@IoDispatcher private val ioDispatcher: Co
                 when {
                     isSuccess() -> data?.content?.let { SUCCESS to it } ?: (MISSING_CONTENT to null)
                     error?.code == MAX_TOKEN_REACHED -> MAX_TOKEN_EXCEEDED to null
+                    // TODO : Detect RATE_LIMIT_EXCEEDED
                     else -> ERROR to null
                 }
             )
