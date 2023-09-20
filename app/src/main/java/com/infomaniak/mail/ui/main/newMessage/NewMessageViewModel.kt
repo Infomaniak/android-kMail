b5edd517625e44a39a49d9d988425dcc2ae87dbe
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2023-09-20 09:53:55 +0200

    Clarifying comment

--- app/src/main/java/com/infomaniak/mail/ui/main/newMessage/NewMessageViewModel.kt
+++ app/src/main/java/com/infomaniak/mail/ui/main/newMessage/NewMessageViewModel.kt
@@ -100,7 +100,7 @@ class NewMessageViewModel @Inject constructor(
 
     // Boolean: For toggleable actions, `false` if the formatting has been removed and `true` if the formatting has been applied.
     val editorAction = SingleLiveEvent<Pair<EditorAction, Boolean?>>()
-    val initResult = MutableLiveData<List<Signature>>()
+    val initResult = MutableLiveData<List<Signature>>() // Needs to trigger every time the Fragment is recreated
     val importedAttachments = MutableLiveData<Pair<MutableList<Attachment>, ImportationResult>>()
     val isSendingAllowed = MutableLiveData(false)
     val externalRecipientCount = MutableLiveData<Pair<String?, Int>>()
