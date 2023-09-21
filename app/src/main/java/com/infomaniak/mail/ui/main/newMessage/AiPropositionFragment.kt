2cc96f13b9b7f1f166126d3ab262ea9fe8e040cb
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2023-09-21 13:24:39 +0200

    New prompt loading behaviour

--- app/src/main/java/com/infomaniak/mail/ui/main/newMessage/AiPropositionFragment.kt
+++ app/src/main/java/com/infomaniak/mail/ui/main/newMessage/AiPropositionFragment.kt
@@ -21,6 +21,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isGone
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
@@ -30,9 +32,13 @@ import com.infomaniak.mail.data.LocalSettings
 import com.infomaniak.mail.data.LocalSettings.AiReplacementDialogVisibility
 import com.infomaniak.mail.databinding.DialogAiReplaceContentBinding
 import com.infomaniak.mail.databinding.FragmentAiPropositionBinding
+import com.infomaniak.mail.ui.main.newMessage.AiViewModel.PropositionStatus
 import com.infomaniak.mail.utils.changeToolbarColorOnScroll
 import com.infomaniak.mail.utils.postfixWithTag
 import dagger.hilt.android.AndroidEntryPoint
+import io.sentry.Sentry
+import io.sentry.SentryLevel
+import kotlinx.coroutines.Job
 import javax.inject.Inject
 import com.infomaniak.lib.core.R as RCore
 
@@ -55,11 +61,17 @@ class AiPropositionFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) = with(binding) {
         super.onViewCreated(view, savedInstanceState)
 
+        setUi()
+
+        resetAiProposition()
+        aiViewModel.generateAiProposition()
+        observeAiProposition()
+    }
+
+    private fun setUi() = with(binding) {
         setToolbar()
 
-        aiViewModel.aiProposition.observe(viewLifecycleOwner) { proposition ->
-            propositionTextView.text = proposition?.second
-        }
+        promptPreview.text = aiViewModel.aiPrompt
 
         insertPropositionButton.setOnClickListener {
             val doNotAskAgain = localSettings.aiReplacementDialogVisibility == AiReplacementDialogVisibility.HIDE
@@ -111,5 +123,43 @@ class AiPropositionFragment : Fragment() {
             .setPositiveButton(R.string.aiReplacementDialogPositiveButton) { _, _ -> onPositiveButtonClicked() }
             .setNegativeButton(RCore.string.buttonCancel, null)
             .create()
+    }
+
+    private fun resetAiProposition() {
+        aiViewModel.aiProposition.value = null
+    }
+
+    private fun observeAiProposition() = with(binding) {
+        aiViewModel.aiProposition.observe(viewLifecycleOwner) { proposition ->
+            if (proposition == null) return@observe
+
+            val (status, body) = proposition
+
+            when (status) {
+                PropositionStatus.SUCCESS -> {
+                    propositionTextView.apply {
+                        text = body
+                        isVisible = true
+                    }
+                    promptPreview.isGone = true
+
+                    buttonLayout.isVisible = true
+                    generationLoader.isGone = true
+                }
+                PropositionStatus.ERROR -> {
+                    // TODO
+                    newMessageViewModel.snackBarManager.setValue(getString(RCore.string.anErrorHasOccurred))
+                }
+                PropositionStatus.MAX_TOKEN_EXCEEDED -> TODO()
+                PropositionStatus.RATE_LIMIT_EXCEEDED -> TODO()
+                PropositionStatus.MISSING_CONTENT -> {
+                    Sentry.withScope { scope ->
+                        scope.level = SentryLevel.ERROR
+                        Sentry.captureMessage("AI call succeeded but no content returned")
+                    }
+                    // TODO
+                }
+            }
+        }
     }
 }
