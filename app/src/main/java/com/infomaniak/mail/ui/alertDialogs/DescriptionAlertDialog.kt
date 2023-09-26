bc6e7602cfa78d47467b38461f600a63fc15aa4a
Fabian Devel <fabian.devel@infomaniak.com>
2023-09-26 10:09:58 +0200

    feat(DescriptionAlertDialog): Add DescriptionAlertDialog class

---
+++ app/src/main/java/com/infomaniak/mail/ui/alertDialogs/DescriptionAlertDialog.kt
@@ -0,0 +1,58 @@
+/*
+ * Infomaniak Mail - Android
+ * Copyright (C) 2023 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.infomaniak.mail.ui.alertDialogs
+
+import android.content.Context
+import androidx.annotation.StringRes
+import com.infomaniak.lib.core.utils.hideProgress
+import com.infomaniak.mail.R
+import dagger.hilt.android.qualifiers.ActivityContext
+import javax.inject.Inject
+
+class DescriptionAlertDialog @Inject constructor(
+    @ActivityContext private val activityContext: Context,
+) : BaseAlertDialog(activityContext) {
+
+    fun show(
+        title: String,
+        description: CharSequence?,
+        @StringRes confirmButtonText: Int? = null,
+        displayCancelButton: Boolean = true,
+        displayLoader: Boolean = true,
+        onPositiveButtonClicked: () -> Unit,
+        onDismissed: (() -> Unit)? = null,
+    ) = showDialog(
+        title,
+        description,
+        confirmButtonText,
+        displayCancelButton,
+        displayLoader,
+        onPositiveButtonClicked,
+        onDismissed,
+    )
+
+    fun resetLoadingAndDismiss() = with(alertDialog) {
+        if (isShowing) {
+            dismiss()
+            setCancelable(true)
+            positiveButton.hideProgress(R.string.buttonCreate)
+            negativeButton.isEnabled = true
+        }
+    }
+
+}
