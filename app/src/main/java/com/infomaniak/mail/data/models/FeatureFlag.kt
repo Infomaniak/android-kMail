19a146abdb2175ba9562c4dd12566b29a9e474fd
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2023-09-27 08:42:51 +0200

    Apply suggestions from code review

--- app/src/main/java/com/infomaniak/mail/data/models/FeatureFlag.kt
+++ app/src/main/java/com/infomaniak/mail/data/models/FeatureFlag.kt
@@ -1,6 +1,6 @@
 /*
  * Infomaniak Mail - Android
- * Copyright (C) 2022-2023 Infomaniak Network SA
+ * Copyright (C) 2023 Infomaniak Network SA
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -19,7 +19,6 @@ package com.infomaniak.mail.data.models
 
 import io.realm.kotlin.types.RealmObject
 import io.realm.kotlin.types.annotations.PrimaryKey
-
 
 class FeatureFlag : RealmObject {
     @PrimaryKey
