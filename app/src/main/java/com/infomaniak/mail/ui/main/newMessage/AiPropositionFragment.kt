eca6437d3f9711999ae18fd3f4f3a0367d6f3186
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2023-10-02 14:11:06 +0200

    Fix missing keyboard when modify prompt

--- app/src/main/java/com/infomaniak/mail/ui/main/newMessage/AiPropositionFragment.kt
+++ app/src/main/java/com/infomaniak/mail/ui/main/newMessage/AiPropositionFragment.kt
@@ -24,6 +24,7 @@ import android.view.ViewGroup
 import androidx.core.view.isGone
 import androidx.core.view.isInvisible
 import androidx.core.view.isVisible
+import androidx.core.view.postDelayed
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
@@ -42,7 +43,7 @@ import com.infomaniak.mail.utils.postfixWithTag
 import dagger.hilt.android.AndroidEntryPoint
 import io.sentry.Sentry
 import io.sentry.SentryLevel
-import kotlinx.coroutines.Job
+import kotlinx.coroutines.*
 import javax.inject.Inject
 import com.infomaniak.lib.core.R as RCore
 
@@ -130,7 +131,6 @@ class AiPropositionFragment : Fragment() {
             aiViewModel.aiProposition.value = null
             currentRequestJob = aiViewModel.performShortcut(shortcut)
         }
-
     }
 
     private fun Fragment.createReplaceContentDialog(
