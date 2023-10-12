70437f1c9f4f50bf929beda66e7cf03e135ce980
Kevin Boulongne <kevin.boulongne@infomaniak.com>
2023-10-12 11:20:50 +0200

    Split SyncAutoConfigActivity into 3 Fragments

---
+++ app/src/main/java/com/infomaniak/mail/ui/sync/SyncOnboardingFragment.kt
@@ -0,0 +1,54 @@
+/*
+ * Infomaniak Mail - Android
+ * Copyright (C) 2023 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.infomaniak.mail.ui.sync
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import com.infomaniak.lib.core.utils.safeBinding
+import com.infomaniak.lib.core.utils.safeNavigate
+import com.infomaniak.mail.databinding.FragmentSyncOnboardingBinding
+
+class SyncOnboardingFragment : Fragment() {
+
+    private var binding: FragmentSyncOnboardingBinding by safeBinding()
+    private val syncAutoConfigViewModel: SyncAutoConfigViewModel by activityViewModels()
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        return FragmentSyncOnboardingBinding.inflate(inflater, container, false).also { binding = it }.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupClickListener()
+    }
+
+    private fun setupClickListener() {
+
+        val direction = if (syncAutoConfigViewModel.isSyncAppInstalled()) {
+            SyncOnboardingFragmentDirections.actionSyncOnboardingFragmentToSyncStartFragment()
+        } else {
+            SyncOnboardingFragmentDirections.actionSyncOnboardingFragmentToSyncInstallFragment()
+        }
+
+        binding.continueButton.setOnClickListener { safeNavigate(direction) }
+    }
+}
