8aec951816a33533712c652bec78703406a03c8c
Fabian Devel <fabian.devel@infomaniak.com>
2023-10-18 08:31:12 +0200

    fix(NPE): Manually set binding to access threadAdapter in onDestroyView

--- app/src/main/java/com/infomaniak/mail/ui/main/thread/ThreadFragment.kt
+++ app/src/main/java/com/infomaniak/mail/ui/main/thread/ThreadFragment.kt
@@ -81,7 +81,9 @@ class ThreadFragment : Fragment() {
     @Inject
     lateinit var localSettings: LocalSettings
 
-    private var binding: FragmentThreadBinding by safeBinding()
+    private var _binding: FragmentThreadBinding? = null
+    private val binding get() = _binding!! // This property is only valid between onCreateView and onDestroyView
+
     private val navigationArgs: ThreadFragmentArgs by navArgs()
     private val mainViewModel: MainViewModel by activityViewModels()
     private val threadViewModel: ThreadViewModel by viewModels()
@@ -108,7 +110,7 @@ class ThreadFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         return FragmentThreadBinding.inflate(inflater, container, false).also {
-            binding = it
+            _binding = it
             requireActivity().window.statusBarColor = requireContext().getColor(R.color.backgroundColor)
         }.root
     }
@@ -147,6 +149,7 @@ class ThreadFragment : Fragment() {
     override fun onDestroyView() {
         threadAdapter.resetCallbacks()
         super.onDestroyView()
+        _binding = null
     }
 
     private fun observeSubjectUpdateTriggers() {
