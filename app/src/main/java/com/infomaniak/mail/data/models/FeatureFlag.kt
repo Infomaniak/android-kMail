19a630e577c59eff874709b4b0bf26545dc1e645
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2023-11-01 11:04:56 +0100

    Add warning comment

--- app/src/main/java/com/infomaniak/mail/data/models/FeatureFlag.kt
+++ app/src/main/java/com/infomaniak/mail/data/models/FeatureFlag.kt
@@ -18,6 +18,7 @@
 package com.infomaniak.mail.data.models
 
 
+// The field apiName is also used to store the enum in Realm
 enum class FeatureFlag(val apiName: String) {
     AI("ai-mail-composer")
 }
