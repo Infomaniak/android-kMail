fc4a7408b4d9f3eb74792d26fef7229f5ff8c55b
Kevin Boulongne <kevin.boulongne@infomaniak.com>
2024-03-19 07:35:05 +0100

    Update all copyrights

--- app/src/main/java/com/infomaniak/mail/receivers/RebootReceiver.kt
+++ app/src/main/java/com/infomaniak/mail/receivers/RebootReceiver.kt
@@ -1,6 +1,6 @@
 /*
  * Infomaniak Mail - Android
- * Copyright (C) 2022 Infomaniak Network SA
+ * Copyright (C) 2022-2024 Infomaniak Network SA
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
