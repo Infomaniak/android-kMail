f020d8b2e850d7351861bb2257e7b64cd915b744
Kevin Boulongne <kevin.boulongne@infomaniak.com>
2024-04-05 09:11:11 +0200

    Revert to use previous `aiOutputToInsert`

--- app/src/main/java/com/infomaniak/mail/ui/newMessage/AiViewModel.kt
+++ app/src/main/java/com/infomaniak/mail/ui/newMessage/AiViewModel.kt
@@ -23,7 +23,6 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.infomaniak.lib.core.models.ApiResponse
-import com.infomaniak.lib.core.utils.SingleLiveEvent
 import com.infomaniak.mail.R
 import com.infomaniak.mail.data.LocalSettings
 import com.infomaniak.mail.data.api.ApiRepository
@@ -60,7 +59,6 @@ class AiViewModel @Inject constructor(
     var previousMessageBodyPlainText by aiSharedData::previousMessageBodyPlainText
 
     val aiPropositionStatusLiveData = MutableLiveData<PropositionStatus>()
-    val aiOutputToInsert = SingleLiveEvent<Pair<String?, String>>()
 
     fun generateNewAiProposition(
         currentMailboxUuid: String,
