923e239fd1215d3588abcdf354f190e8ad56ab65
Vincent TE <vincent.te@infomaniak.com>
2024-04-24 08:53:54 +0200

    Dismiss group notification based on active notification size

--- app/src/main/java/com/infomaniak/mail/utils/FetchMessagesManager.kt
+++ app/src/main/java/com/infomaniak/mail/utils/FetchMessagesManager.kt
@@ -139,6 +139,12 @@ class FetchMessagesManager @Inject constructor(
             }
         }
 
+        if (notificationManagerCompat.activeNotifications.isEmpty()) {
+            // We don't need to display a group notification saying "0 new messages" so if we don't have any active notifications,
+            // We dismiss the group notification.
+            notificationManagerCompat.cancel(mailbox.notificationGroupId)
+        }
+
         if (threadsWithNewMessages.isEmpty()) {
             SentryDebug.sendFailedNotification(
                 reason = "No new Message",
@@ -150,10 +156,6 @@ class FetchMessagesManager @Inject constructor(
             )
 
             realm.close()
-
-            // We don't need to display a group notification saying "0 new messages" so if we don't have any new unread messages,
-            // We dismiss the group notification.
-            notificationManagerCompat.cancel(mailbox.notificationGroupId)
             return
         }
 
