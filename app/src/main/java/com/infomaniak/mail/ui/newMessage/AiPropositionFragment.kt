a78f08c7f86df8457f5d25c3da12ae2698647bc5
Tommy DI LUNA <tommy.diluna@infomaniak.com>
2024-05-24 11:11:24 +0200

    Add more customisation to InformationBlockView and change NoValidMailboxesFragment layout
    It now has three optional elements: a title, a description, and a button.

--- app/src/main/java/com/infomaniak/mail/ui/newMessage/AiPropositionFragment.kt
+++ app/src/main/java/com/infomaniak/mail/ui/newMessage/AiPropositionFragment.kt
@@ -296,7 +296,7 @@ class AiPropositionFragment : Fragment() {
     }
 
     private fun displayError(status: PropositionStatus) {
-        binding.errorBlock.setText(status.errorRes!!)
+        binding.errorBlock.setTitle(status.errorRes!!)
         setUiVisibilityState(UiState.ERROR)
     }
 
