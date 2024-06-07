167de243e4e009dccb93bfa3029af15c7c20f30f
Kevin Boulongne <kevin.boulongne@infomaniak.com>
2024-06-07 13:25:02 +0200

    Clean code

--- app/src/main/java/com/infomaniak/mail/ui/main/move/MoveFragment.kt
+++ app/src/main/java/com/infomaniak/mail/ui/main/move/MoveFragment.kt
@@ -38,6 +38,7 @@ import com.infomaniak.mail.R
 import com.infomaniak.mail.data.models.Folder
 import com.infomaniak.mail.data.models.Folder.FolderRole
 import com.infomaniak.mail.databinding.FragmentMoveBinding
+import com.infomaniak.mail.ui.main.menuDrawer.MenuFoldersFragment
 import com.infomaniak.mail.utils.extensions.handleEditorSearchAction
 import com.infomaniak.mail.utils.extensions.setOnClearTextClickListener
 import com.infomaniak.mail.utils.extensions.setSystemBarsColors
