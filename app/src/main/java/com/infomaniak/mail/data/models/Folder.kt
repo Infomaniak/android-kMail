e53ba8da3867188e1dd39f4874b19c296adc9a67
Kevin Boulongne <kevin.boulongne@infomaniak.com>
2024-06-13 10:39:14 +0200

    Move Folder cloning to a coroutine in the VM

--- app/src/main/java/com/infomaniak/mail/data/models/Folder.kt
+++ app/src/main/java/com/infomaniak/mail/data/models/Folder.kt
@@ -113,6 +113,9 @@ class Folder : RealmObject, Cloneable {
     val isRoot: Boolean
         inline get() = !path.contains(separator)
 
+    val isRootAndCustom: Boolean
+        inline get() = role == null && isRoot
+
     fun initLocalValues(
         lastUpdatedAt: RealmInstant?,
         cursor: String?,
