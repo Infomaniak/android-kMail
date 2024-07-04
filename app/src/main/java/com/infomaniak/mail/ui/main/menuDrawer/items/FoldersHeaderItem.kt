39147b5c0331dca7973e5209d9f4359e45142ec9
Kevin Boulongne <kevin.boulongne@infomaniak.com>
2024-07-04 10:02:29 +0200

    Move each MenuDrawerAdapter's items binding to its own file

---
+++ app/src/main/java/com/infomaniak/mail/ui/main/menuDrawer/items/FoldersHeaderItem.kt
@@ -0,0 +1,54 @@
+/*
+ * Infomaniak Mail - Android
+ * Copyright (C) 2024 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.infomaniak.mail.ui.main.menuDrawer.items
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.viewbinding.ViewBinding
+import com.infomaniak.mail.R
+import com.infomaniak.mail.databinding.ItemMenuDrawerCustomFoldersHeaderBinding
+
+object FoldersHeaderItem {
+
+    @Suppress("MayBeConstant")
+    val viewType = R.layout.view_menu_drawer_dropdown
+
+    fun binding(inflater: LayoutInflater, parent: ViewGroup): ViewBinding {
+        return ItemMenuDrawerCustomFoldersHeaderBinding.inflate(inflater, parent, false)
+    }
+
+    fun display(
+        binding: ViewBinding,
+        onCustomFoldersHeaderClicked: (Boolean) -> Unit,
+        onCreateFolderClicked: () -> Unit,
+    ) {
+        binding as ItemMenuDrawerCustomFoldersHeaderBinding
+
+        Log.d("Bind", "Bind Custom Folders header")
+        binding.displayCustomFoldersHeader(onCustomFoldersHeaderClicked, onCreateFolderClicked)
+    }
+
+    private fun ItemMenuDrawerCustomFoldersHeaderBinding.displayCustomFoldersHeader(
+        onCustomFoldersHeaderClicked: (Boolean) -> Unit,
+        onCreateFolderClicked: () -> Unit,
+    ) = with(root) {
+        setOnClickListener { onCustomFoldersHeaderClicked(isCollapsed) }
+        setOnActionClickListener { onCreateFolderClicked() }
+    }
+}
---
+++ app/src/main/java/com/infomaniak/mail/ui/main/menuDrawer/items/MailboxesHeaderItem.kt
@@ -0,0 +1,90 @@
+/*
+ * Infomaniak Mail - Android
+ * Copyright (C) 2024 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.infomaniak.mail.ui.main.menuDrawer.items
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.viewbinding.ViewBinding
+import com.infomaniak.mail.R
+import com.infomaniak.mail.data.models.mailbox.Mailbox
+import com.infomaniak.mail.databinding.ItemMenuDrawerMailboxesHeaderBinding
+import com.infomaniak.mail.utils.extensions.toggleChevron
+
+object MailboxesHeaderItem {
+
+    @Suppress("MayBeConstant")
+    val viewType = R.layout.item_menu_drawer_mailboxes_header
+
+    fun binding(inflater: LayoutInflater, parent: ViewGroup): ViewBinding {
+        return ItemMenuDrawerMailboxesHeaderBinding.inflate(inflater, parent, false)
+    }
+
+    fun display(
+        item: Any,
+        binding: ViewBinding,
+        onMailboxesHeaderClicked: () -> Unit,
+    ) {
+        item as MailboxesHeader
+        binding as ItemMenuDrawerMailboxesHeaderBinding
+
+        Log.d("Bind", "Bind Mailboxes header")
+        binding.displayMailboxesHeader(item, onMailboxesHeaderClicked)
+    }
+
+    fun displayWithPayload(
+        item: Any,
+        binding: ViewBinding,
+    ) {
+        item as MailboxesHeader
+        binding as ItemMenuDrawerMailboxesHeaderBinding
+
+        Log.d("Bind", "Bind Mailboxes header because of collapse change")
+        binding.updateCollapseState(item)
+    }
+
+    private fun ItemMenuDrawerMailboxesHeaderBinding.displayMailboxesHeader(
+        header: MailboxesHeader,
+        onMailboxesHeaderClicked: () -> Unit,
+    ) = with(header) {
+        mailboxSwitcherText.text = mailbox?.email
+
+        mailboxSwitcher.apply {
+            isClickable = hasMoreThanOneMailbox
+            isFocusable = hasMoreThanOneMailbox
+        }
+
+        mailboxExpandButton.isVisible = hasMoreThanOneMailbox
+
+        setMailboxSwitcherTextAppearance(isExpanded)
+
+        root.setOnClickListener { onMailboxesHeaderClicked() }
+    }
+
+    private fun ItemMenuDrawerMailboxesHeaderBinding.updateCollapseState(header: MailboxesHeader) = with(header) {
+        mailboxExpandButton.toggleChevron(!isExpanded)
+        setMailboxSwitcherTextAppearance(isExpanded)
+    }
+
+    private fun ItemMenuDrawerMailboxesHeaderBinding.setMailboxSwitcherTextAppearance(isOpen: Boolean) {
+        mailboxSwitcherText.setTextAppearance(if (isOpen) R.style.BodyMedium_Accent else R.style.BodyMedium)
+    }
+
+    data class MailboxesHeader(val mailbox: Mailbox?, val hasMoreThanOneMailbox: Boolean, val isExpanded: Boolean)
+}
