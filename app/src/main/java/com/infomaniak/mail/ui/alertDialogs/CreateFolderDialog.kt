a83b0b75eaba6c74ed534cd4da578988a6305fb0
Kevin Boulongne <kevin.boulongne@infomaniak.com>
2024-07-04 10:40:29 +0200

    Factorize CreateFolderDialog creation

---
+++ app/src/main/java/com/infomaniak/mail/ui/alertDialogs/CreateFolderDialog.kt
@@ -0,0 +1,54 @@
+/*
+ * Infomaniak Mail - Android
+ * Copyright (C) 2024 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.infomaniak.mail.ui.alertDialogs
+
+import android.content.Context
+import androidx.annotation.StringRes
+import com.infomaniak.mail.MatomoMail.trackCreateFolderEvent
+import com.infomaniak.mail.R
+import com.infomaniak.mail.data.cache.mailboxContent.FolderController
+import com.infomaniak.mail.di.IoDispatcher
+import com.infomaniak.mail.utils.extensions.getFolderCreationError
+import dagger.hilt.android.qualifiers.ActivityContext
+import dagger.hilt.android.scopes.ActivityScoped
+import kotlinx.coroutines.CoroutineDispatcher
+import javax.inject.Inject
+
+@ActivityScoped
+class CreateFolderDialog @Inject constructor(
+    @ActivityContext private val activityContext: Context,
+    @IoDispatcher private val ioDispatcher: CoroutineDispatcher,
+    private val folderController: FolderController,
+) : InputAlertDialog(activityContext, ioDispatcher) {
+
+    fun show(@StringRes confirmButtonText: Int = R.string.buttonCreate) = show(
+        title = R.string.newFolderDialogTitle,
+        hint = R.string.newFolderDialogHint,
+        confirmButtonText = confirmButtonText,
+    )
+
+    fun setCallbacks(onPositiveButtonClicked: (String) -> Unit) = setCallbacks(
+        onPositiveButtonClicked = { folderName ->
+            activityContext.trackCreateFolderEvent("confirm")
+            onPositiveButtonClicked(folderName)
+        },
+        onErrorCheck = { folderName ->
+            activityContext.getFolderCreationError(folderName, folderController)
+        },
+    )
+}
