b720075147d08da4bdd9afdd29a52bcb8193c77c
Fabian Devel <fabian.devel@infomaniak.com>
2024-07-15 13:32:53 +0200

    chores(Sentry): Remove useless Sentry

--- app/src/main/java/com/infomaniak/mail/utils/FetchMessagesManager.kt
+++ app/src/main/java/com/infomaniak/mail/utils/FetchMessagesManager.kt
@@ -183,19 +183,11 @@ class FetchMessagesManager @Inject constructor(
             )
             return
         }
-        if (message.isSeen) {
-            // If the Message has already been seen before receiving the Notification, we don't want to display it.
-            // We can leave safely.
-            SentryDebug.sendFailedNotification(
-                reason = "Message already seen",
-                sentryLevel = SentryLevel.INFO,
-                userId = userId,
-                mailboxId = mailbox.mailboxId,
-                messageUid = sentryMessageUid,
-                mailbox = mailbox,
-            )
-            return
-        }
+
+        // If the Message has already been seen before receiving the Notification, we don't want to display it.
+        // We can leave safely.
+        // It can happened very simply if you open a message on the webmail for example
+        if (message.isSeen) return
 
         val formattedPreview = message.preview.ifBlank { null }?.let { "\n${it.trim()}" } ?: ""
         val body = if (message.body?.value.isNullOrBlank()) {
