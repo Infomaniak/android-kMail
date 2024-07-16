4013c0557cf4c750d2aa3f97f6379b2e79efb7ef
Kevin Boulongne <kevin.boulongne@infomaniak.com>
2024-07-16 11:14:28 +0200

    Rename generic `getFoldersAsync()` into more specific `getRootFoldersAsync()`

--- app/src/main/java/com/infomaniak/mail/ui/main/search/SearchViewModel.kt
+++ app/src/main/java/com/infomaniak/mail/ui/main/search/SearchViewModel.kt
@@ -79,7 +79,7 @@ class SearchViewModel @Inject constructor(
     var currentSearchQuery: String = ""
         private set
 
-    val foldersLive = folderController.getFoldersAsync()
+    val foldersLive = folderController.getRootFoldersAsync()
         .map { it.list.flattenFolderChildren() }
         .asLiveData(ioCoroutineContext)
 
