cfb8e26327a9306732c68ee5f96a5e53bf9ab5b9
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2024-08-13 10:35:54 +0200

    Fix wrongly renamed view

--- app/src/main/java/com/infomaniak/mail/ui/main/folder/ThreadListMultiSelection.kt
+++ app/src/main/java/com/infomaniak/mail/ui/main/folder/ThreadListMultiSelection.kt
@@ -141,7 +141,7 @@ class ThreadListMultiSelection {
     private fun displaySelectionToolbar(isMultiSelectOn: Boolean) = with(threadListFragment.binding) {
         val autoTransition = AutoTransition()
         autoTransition.duration = TOOLBAR_FADE_DURATION
-        TransitionManager.beginDelayedTransition(formatOptionsScrollView, autoTransition)
+        TransitionManager.beginDelayedTransition(toolbarLayout, autoTransition)
 
         toolbar.isGone = isMultiSelectOn
         toolbarSelection.isVisible = isMultiSelectOn
