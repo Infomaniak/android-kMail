71cb3f4a259292425bbcf2f25adb9a7d164986b0
Kevin Boulongne <kevin.boulongne@infomaniak.com>
2024-09-24 13:45:21 +0200

    Update UI when read/unread status change + Fix ViewHolder recycling that wasn't hiding the mailAddress

--- app/src/main/java/com/infomaniak/mail/ui/main/folder/ThreadListAdapter.kt
+++ app/src/main/java/com/infomaniak/mail/ui/main/folder/ThreadListAdapter.kt
@@ -212,9 +212,7 @@ class ThreadListAdapter @Inject constructor(
         refreshCachedSelectedPosition(thread.uid, position) // If item changed position, update cached position.
         setupThreadDensityDependentUi()
         displayAvatar(thread)
-
-        displaySpamSpecificUI(thread)
-
+        displaySpamSpecificUi(thread)
         displayFolderName(thread)
 
         // This method is only useful for old Threads already stored in Realm, where they
@@ -269,11 +267,14 @@ class ThreadListAdapter @Inject constructor(
         updateSelectedUi(thread)
     }
 
-    private fun CardviewThreadItemBinding.displaySpamSpecificUI(thread: Thread) {
-        if (folderRole == FolderRole.SPAM) {
-            mail.text = thread.from.first().quotedEmail()
-            mail.isVisible = true
+    private fun CardviewThreadItemBinding.displaySpamSpecificUi(thread: Thread) {
+        val (mailAddressText, isVisible) = if (folderRole == FolderRole.SPAM) {
+            thread.from.first().quotedEmail() to true
+        } else {
+            "" to false
         }
+        mailAddress.text = mailAddressText
+        mailAddress.isVisible = isVisible
     }
 
     private fun CardviewThreadItemBinding.displayFolderName(thread: Thread) {
@@ -476,25 +477,27 @@ class ThreadListAdapter @Inject constructor(
         newMailBullet.isInvisible = true
 
         expeditor.setBodyRegularSecondary()
+        mailAddress.setBodyRegularSecondary()
         mailSubject.setBodyRegularSecondary()
 
         threadCountText.setTextAppearance(R.style.Label_Secondary)
         threadCountCard.strokeColor = context.getColor(R.color.threadCountBorderRead)
     }
 
-    private fun TextView.setBodyRegularSecondary() {
-        setTextAppearance(R.style.Body)
-        setTextColor(context.getColor(R.color.secondaryTextColor))
-    }
-
     private fun CardviewThreadItemBinding.setThreadUiUnread() {
         newMailBullet.isVisible = true
 
         expeditor.setBodyMediumPrimary()
+        mailAddress.setBodyMediumPrimary()
         mailSubject.setBodyMediumPrimary()
 
         threadCountText.setTextAppearance(R.style.LabelMedium)
         threadCountCard.strokeColor = context.getColor(R.color.threadCountBorderUnread)
+    }
+
+    private fun TextView.setBodyRegularSecondary() {
+        setTextAppearance(R.style.Body)
+        setTextColor(context.getColor(R.color.secondaryTextColor))
     }
 
     private fun TextView.setBodyMediumPrimary() {
