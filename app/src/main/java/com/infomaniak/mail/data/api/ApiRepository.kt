49baa7913cf00abf491a031777263b01871e0134
Nicolas Bourdin <nicolas.bourdin@infomaniak.com>
2024-10-29 13:54:08 +0100

    refactor: Share code of 'reportDisplayProblem'

--- app/src/main/java/com/infomaniak/mail/data/api/ApiRepository.kt
+++ app/src/main/java/com/infomaniak/mail/data/api/ApiRepository.kt
@@ -435,6 +435,15 @@ object ApiRepository : ApiRepositoryCore() {
         return callApi(url = ApiRoutes.shareLink(mailboxUuid, folderId, mailId), method = POST)
     }
 
+    fun getDownloadedAttachment(mailboxUuid: String, folderId: String, shortUid: Int): Response {
+        val request = Request.Builder().url(ApiRoutes.downloadMessage(mailboxUuid, folderId, shortUid))
+            .headers(HttpUtils.getHeaders(null))
+            .get()
+            .build()
+
+        return HttpClient.okHttpClient.newCall(request).execute()
+    }
+
     /**
      * Create batches of the given values to perform the given request
      * @param values Data to batch
