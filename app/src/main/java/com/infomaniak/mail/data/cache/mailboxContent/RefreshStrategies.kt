213167b3728afb91da3b05a6e6d2d9a562d86fe1
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2025-03-04 08:44:38 +0100

    refactor: Clean some code

--- app/src/main/java/com/infomaniak/mail/data/cache/mailboxContent/RefreshStrategies.kt
+++ app/src/main/java/com/infomaniak/mail/data/cache/mailboxContent/RefreshStrategies.kt
@@ -31,7 +31,7 @@ import kotlinx.coroutines.ensureActive
 
 interface RefreshStrategy {
     fun queryFolderThreads(folderId: String, realm: TypedRealm): List<Thread>
-    fun otherFolderRolesToQueryThreads(): List<Folder.FolderRole> // TODO: Couple it tighter with queryFolderThreads()?
+    fun otherFolderRolesToQueryThreads(): List<Folder.FolderRole>
 
     fun getMessageFromShortUid(shortUid: String, folderId: String, realm: TypedRealm): Message?
 
@@ -39,7 +39,7 @@ interface RefreshStrategy {
      * @return The list of impacted threads that have changed and need to be recomputed. The list of impacted threads will also be
      * used to determine what folders need to have their unread count updated. If an extra folder needs its unread count updated
      * but no thread has that extra folder as [Thread.folderId], you can define the extra folder you want inside
-     * [addFolderToImpactedFolders] as you they are inserted inside the list of impacted folders.
+     * [addFolderToImpactedFolders] as they will be inserted inside the list of impacted folders.
      */
     fun processDeletedMessage(
         scope: CoroutineScope,
