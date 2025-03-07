56cc86edfbde0756a793911a3eefe8216e2cccbd
Kevin Boulongne <kevin.boulongne@infomaniak.com>
2025-03-07 09:10:09 +0100

    fix: The `impactedThreadsManaged` are now correctly updated

--- app/src/main/java/com/infomaniak/mail/data/cache/mailboxContent/refreshStrategies/RefreshStrategy.kt
+++ app/src/main/java/com/infomaniak/mail/data/cache/mailboxContent/refreshStrategies/RefreshStrategy.kt
@@ -64,6 +64,7 @@ interface RefreshStrategy {
         scope: CoroutineScope,
         remoteMessage: Message,
         isConversationMode: Boolean,
+        impactedThreadsManaged: MutableSet<Thread>,
         realm: MutableRealm,
-    ): Set<Thread>
+    )
 }
