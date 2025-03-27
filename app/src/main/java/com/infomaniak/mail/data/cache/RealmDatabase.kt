b5f16c6412534a7bd4ba5d14ee6da47fead32775
Vincent TE <vincent.te@infomaniak.com>
2025-03-27 15:00:42 +0100

    chore: Fix realm version

--- app/src/main/java/com/infomaniak/mail/data/cache/RealmDatabase.kt
+++ app/src/main/java/com/infomaniak/mail/data/cache/RealmDatabase.kt
@@ -164,7 +164,7 @@ object RealmDatabase {
         //region Configurations versions
         const val USER_INFO_SCHEMA_VERSION = 3L
         const val MAILBOX_INFO_SCHEMA_VERSION = 9L
-        const val MAILBOX_CONTENT_SCHEMA_VERSION = 26L
+        const val MAILBOX_CONTENT_SCHEMA_VERSION = 27L
         //endregion
 
         //region Configurations names
