44efcb908b5852f3c1a3b1108111ee139029c583
Fabian DEVEL <fabian.devel@infomaniak.com>
2025-05-19 14:42:30 +0200

    chore(Api35): Clean imports

--- app/src/main/java/com/infomaniak/mail/ui/newMessage/AiPromptFragment.kt
+++ app/src/main/java/com/infomaniak/mail/ui/newMessage/AiPromptFragment.kt
@@ -29,13 +29,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.withStarted
-import com.infomaniak.core.fragmentnavigation.safelyNavigate
 import com.infomaniak.lib.core.utils.*
-import com.infomaniak.mail.MatomoMail.trackEvent
-import com.infomaniak.lib.core.utils.safeBinding
-import com.infomaniak.lib.core.utils.setMarginsRelative
-import com.infomaniak.lib.core.utils.showKeyboard
-import com.infomaniak.lib.core.utils.toPx
 import com.infomaniak.mail.R
 import com.infomaniak.mail.data.LocalSettings
 import com.infomaniak.mail.databinding.FragmentAiPromptBinding
