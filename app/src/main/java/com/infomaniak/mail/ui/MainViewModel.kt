76d45f5ee182d2a9a56effe00747d7e0e574e922
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2025-05-20 17:06:02 +0200

    feat: Finish replacing messages acccess with getDisplayMessages() in MessageControler

--- app/src/main/java/com/infomaniak/mail/ui/MainViewModel.kt
+++ app/src/main/java/com/infomaniak/mail/ui/MainViewModel.kt
@@ -924,7 +924,9 @@ class MainViewModel @Inject constructor(
     }
 
     private fun getMessagesToMarkAsUnseen(threads: List<Thread>, message: Message?) = when (message) {
-        null -> threads.flatMap(messageController::getLastMessageAndItsDuplicatesToExecuteAction)
+        null -> threads.flatMap { thread ->
+            messageController.getLastMessageAndItsDuplicatesToExecuteAction(thread, featureFlagsLive.value)
+        }
         else -> messageController.getMessageAndDuplicates(threads.first(), message)
     }
 
@@ -990,7 +992,9 @@ class MainViewModel @Inject constructor(
     }
 
     private fun getMessagesToFavorite(threads: List<Thread>, message: Message?) = when (message) {
-        null -> threads.flatMap(messageController::getLastMessageAndItsDuplicatesToExecuteAction)
+        null -> threads.flatMap { thread ->
+            messageController.getLastMessageAndItsDuplicatesToExecuteAction(thread, featureFlagsLive.value)
+        }
         else -> messageController.getMessageAndDuplicates(threads.first(), message)
     }
 
@@ -1437,7 +1441,7 @@ class MainViewModel @Inject constructor(
     }
 
     private fun threadHasOnlyOneMessageLeft(threadUid: String): Boolean {
-        return messageController.getMessagesCountInThread(threadUid, mailboxContentRealm()) == 1
+        return messageController.getMessagesCountInThread(threadUid, featureFlagsLive.value, mailboxContentRealm()) == 1
     }
 
     fun shareThreadUrl(messageUid: String) {
