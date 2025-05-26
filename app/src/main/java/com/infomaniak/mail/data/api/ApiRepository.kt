f1d8f1970c3b12401bae9416b94590b56f1ac87a
Fabian DEVEL <fabian.devel@infomaniak.com>
2025-05-26 08:39:16 +0200

    feat(Encryption): Add api route to know if contacts can be encrypted

--- app/src/main/java/com/infomaniak/mail/data/api/ApiRepository.kt
+++ app/src/main/java/com/infomaniak/mail/data/api/ApiRepository.kt
@@ -138,6 +138,10 @@ object ApiRepository : ApiRepositoryCore() {
         return callApi(ApiRoutes.requestMailboxPassword(mailboxHostingId, mailboxName), POST)
     }
 
+    fun isInfomaniakMailbox(emails: List<String>): ApiResponse<Map<String, Boolean>> {
+        return callApi(ApiRoutes.isInfomaniakMailboxes(emails), GET)
+    }
+
     fun getFolders(mailboxUuid: String): ApiResponse<List<Folder>> = callApi(ApiRoutes.folders(mailboxUuid), GET)
 
     fun createFolder(mailboxUuid: String, name: String): ApiResponse<Folder> {
