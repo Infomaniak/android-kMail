b8a3ec0960965a86ff200b2ec502c0a155bd359c
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2025-05-30 16:46:49 +0200

    feat: Send emoji when emoji is picked through emoji picker

--- app/src/main/java/com/infomaniak/mail/ui/MainActivity.kt
+++ app/src/main/java/com/infomaniak/mail/ui/MainActivity.kt
@@ -341,7 +341,7 @@ class MainActivity : BaseActivity() {
                         showSavedDraftSnackbar(associatedMailboxUuid, remoteDraftUuid)
                     }
                 }
-                DraftAction.SEND -> {
+                DraftAction.SEND, DraftAction.SEND_REACTION -> {
                     showSentDraftSnackbar()
                 }
                 DraftAction.SCHEDULE -> {
