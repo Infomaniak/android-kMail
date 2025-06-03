9aa678be194b42adbdb60b902813579dabffd575
Fabian DEVEL <fabian.devel@infomaniak.com>
2025-06-03 09:38:51 +0200

    feat(Encryption): Update the "exists" api call returns now that it has changed

--- app/src/main/java/com/infomaniak/mail/data/api/ApiRepository.kt
+++ app/src/main/java/com/infomaniak/mail/data/api/ApiRepository.kt
@@ -157,7 +157,7 @@ object ApiRepository : ApiRepositoryCore() {
         return callApi(ApiRoutes.requestMailboxPassword(mailboxHostingId, mailboxName), POST)
     }
 
-    suspend fun isInfomaniakMailbox(emails: List<String>): ApiResponse<Map<String, Boolean>> {
+    suspend fun isInfomaniakMailbox(emails: List<String>): ApiResponse<List<MailboxHostingStatus>> {
         return callApi(ApiRoutes.isInfomaniakMailboxes(emails), GET)
     }
 
