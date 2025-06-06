c39764e28bf3870d1f8ddb5d529d789e77a127be
Abdourahamane Boinaidi <abdourahamane.boinaidi@infomaniak.com>
2025-06-06 14:57:02 +0200

    chore: Remove useless import

--- app/src/main/java/com/infomaniak/mail/utils/SharedUtils.kt
+++ app/src/main/java/com/infomaniak/mail/utils/SharedUtils.kt
@@ -41,7 +41,6 @@ import com.infomaniak.mail.data.models.snooze.BatchSnoozeResult
 import com.infomaniak.mail.data.models.thread.Thread
 import com.infomaniak.mail.ui.MainViewModel
 import com.infomaniak.mail.utils.JsoupParserUtil.jsoupParseWithLog
-import com.infomaniak.mail.utils.SharedUtils.Companion.unsnoozeThreadsWithoutRefresh
 import com.infomaniak.mail.utils.extensions.atLeastOneSucceeded
 import com.infomaniak.mail.utils.extensions.getApiException
 import com.infomaniak.mail.utils.extensions.getFoldersIds
