4b24f5e0f51e0cc676942d55c78aa49f04c516d4
Abdourahamane Boinaidi <abdourahamane.boinaidi@infomaniak.com>
2025-06-10 14:46:06 +0200

    fix: Log user with a custom okhttpClient

--- app/src/main/java/com/infomaniak/mail/ui/login/LoginActivity.kt
+++ app/src/main/java/com/infomaniak/mail/ui/login/LoginActivity.kt
@@ -28,6 +28,7 @@ import androidx.navigation.fragment.NavHostFragment
 import com.infomaniak.lib.core.InfomaniakCore
 import com.infomaniak.lib.core.api.ApiController.toApiError
 import com.infomaniak.lib.core.api.InternalTranslatedErrorCode
+import com.infomaniak.lib.core.auth.TokenAuthenticator.Companion.changeAccessToken
 import com.infomaniak.lib.core.models.ApiResponse
 import com.infomaniak.lib.core.models.ApiResponseStatus
 import com.infomaniak.lib.core.networking.HttpClient
@@ -110,7 +111,11 @@ class LoginActivity : AppCompatActivity() {
             if (AccountUtils.getUserById(apiToken.userId) != null) return getErrorResponse(InternalTranslatedErrorCode.UserAlreadyPresent)
 
             InfomaniakCore.bearerToken = apiToken.accessToken
-            val userProfileResponse = Dispatchers.IO { ApiRepository.getUserProfile(HttpClient.okHttpClientNoTokenInterceptor) }
+            val okhttpClient = HttpClient.okHttpClientNoTokenInterceptor.newBuilder().addInterceptor { chain ->
+                val newRequest = changeAccessToken(chain.request(), apiToken)
+                chain.proceed(newRequest)
+            }.build()
+            val userProfileResponse = Dispatchers.IO { ApiRepository.getUserProfile(okhttpClient) }
 
             if (userProfileResponse.result == ApiResponseStatus.ERROR) return userProfileResponse
             if (userProfileResponse.data == null) return getErrorResponse(InternalTranslatedErrorCode.UnknownError)
@@ -120,7 +125,7 @@ class LoginActivity : AppCompatActivity() {
                 this.organizations = arrayListOf()
             }
 
-            val apiResponse = Dispatchers.IO { ApiRepository.getMailboxes(HttpClient.okHttpClientNoTokenInterceptor) }
+            val apiResponse = Dispatchers.IO { ApiRepository.getMailboxes(okhttpClient) }
 
             return when {
                 !apiResponse.isSuccess() -> apiResponse
