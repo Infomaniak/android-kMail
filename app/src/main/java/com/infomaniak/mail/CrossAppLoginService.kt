28895dccd92e1abcc8aaa8ec1841b217eb277bb9
Louis CAD <louis.cognault@infomaniak.com>
2025-06-17 15:51:44 +0200

    chore(CrossAppLogin): Update Core

--- app/src/main/java/com/infomaniak/mail/CrossAppLoginService.kt
+++ app/src/main/java/com/infomaniak/mail/CrossAppLoginService.kt
@@ -5,17 +5,14 @@ import com.infomaniak.mail.data.cache.RealmDatabase
 import com.infomaniak.mail.data.models.AppSettings
 import io.realm.kotlin.ext.query
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.emitAll
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
 
-class CrossAppLoginService : BaseCrossAppLoginService() {
-
-    override val selectedUserIdFlow: Flow<Int> = flow {
+class CrossAppLoginService : BaseCrossAppLoginService(
+    selectedUserIdFlow = flow {
         val flow = RealmDatabase.appSettings().query<AppSettings>().first().asFlow().map { it.obj?.currentUserId ?: -1 }
         emitAll(flow)
     }.flowOn(Dispatchers.IO)
-
-}
+)
