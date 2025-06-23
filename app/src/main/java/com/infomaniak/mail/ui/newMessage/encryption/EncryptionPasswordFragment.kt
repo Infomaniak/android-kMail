c9b8492879d272311496703a3c11120ae5ff26d4
Fabian DEVEL <fabian.devel@infomaniak.com>
2025-06-23 09:56:41 +0200

    feat(EncryptionPassword): Add skeleton version of EncryptionPasswordFragment

--- app/src/main/java/com/infomaniak/mail/ui/newMessage/encryption/EncryptionPasswordFragment.kt
+++ app/src/main/java/com/infomaniak/mail/ui/newMessage/encryption/EncryptionPasswordFragment.kt
@@ -1,4 +1,42 @@
+/*
+ * Infomaniak Mail - Android
+ * Copyright (C) 2025 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.infomaniak.mail.ui.newMessage.encryption
 
-class EncryptionPasswordFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.infomaniak.lib.core.utils.safeBinding
+import com.infomaniak.mail.databinding.FragmentEncryptionPasswordBinding
+
+class EncryptionPasswordFragment : Fragment() {
+
+    private var binding: FragmentEncryptionPasswordBinding by safeBinding()
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        return FragmentEncryptionPasswordBinding.inflate(inflater, container, false).also { binding = it }.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.toolbar.setNavigationOnClickListener { findNavController().popBackStack() }
+    }
 }
