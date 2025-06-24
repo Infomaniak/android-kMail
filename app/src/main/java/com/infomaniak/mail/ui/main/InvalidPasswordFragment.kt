098ddfb391481de78bc9ed735b9feec7015e216b
Kylian Grandy <kylian.grandy@infomaniak.com>
2025-06-24 15:06:49 +0200

    chore: Change Fragment extension to Context extension

--- app/src/main/java/com/infomaniak/mail/ui/main/InvalidPasswordFragment.kt
+++ app/src/main/java/com/infomaniak/mail/ui/main/InvalidPasswordFragment.kt
@@ -63,7 +63,10 @@ class InvalidPasswordFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) = with(binding) {
         super.onViewCreated(view, savedInstanceState)
 
-        enterPasswordDescription2.text = getStringWithBoldArg(R.string.enterPasswordDescription2, navigationArgs.mailboxEmail)
+        enterPasswordDescription2.text = requireContext().getStringWithBoldArg(
+            R.string.enterPasswordDescription2,
+            navigationArgs.mailboxEmail,
+        )
 
         confirmButton.isEnabled = false
 
@@ -94,14 +97,13 @@ class InvalidPasswordFragment : Fragment() {
 
         detachMailbox.setOnClickListener {
             trackInvalidPasswordMailboxEvent("detachMailbox")
-            descriptionDialog.show(
-                title = getString(R.string.popupDetachMailboxTitle),
-                description = getStringWithBoldArg(R.string.popupDetachMailboxDescription, navigationArgs.mailboxEmail),
-                onPositiveButtonClicked = {
+            descriptionDialog.show(title = getString(R.string.popupDetachMailboxTitle), description = requireContext().getStringWithBoldArg(
+                    R.string.popupDetachMailboxDescription,
+                    navigationArgs.mailboxEmail
+                ), onPositiveButtonClicked = {
                     trackInvalidPasswordMailboxEvent("detachMailboxConfirm")
                     invalidPasswordViewModel.detachMailbox()
-                },
-            )
+                },)
         }
 
         requestPasswordButton.setOnClickListener {
