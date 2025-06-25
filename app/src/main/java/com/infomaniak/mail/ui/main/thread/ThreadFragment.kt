72f5db301e5fd3f6378298bc378c939229a9750d
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2025-06-25 15:31:25 +0200

    feat: Fake emoji from emoji picker as well

--- app/src/main/java/com/infomaniak/mail/ui/main/thread/ThreadFragment.kt
+++ app/src/main/java/com/infomaniak/mail/ui/main/thread/ThreadFragment.kt
@@ -631,6 +631,7 @@ class ThreadFragment : Fragment() {
 
     private fun observePickedEmoji() {
         getBackNavigationResult<PickedEmojiPayload>(EmojiPickerBottomSheetDialog.PICKED_EMOJI) { (emoji, messageUid) ->
+            threadViewModel.fakeEmojiReply(emoji, messageUid)
             mainViewModel.sendEmojiReply(emoji, messageUid)
         }
     }
