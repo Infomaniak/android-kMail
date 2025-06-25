ef32c1bd7ae9a1611429b040e6d38272f9224afe
Fabian DEVEL <fabian.devel@infomaniak.com>
2025-06-25 09:32:18 +0200

    chore: Clean code

--- app/src/main/java/com/infomaniak/mail/data/api/ApiRepository.kt
+++ app/src/main/java/com/infomaniak/mail/data/api/ApiRepository.kt
@@ -67,6 +67,7 @@ import com.infomaniak.mail.data.models.getMessages.MessageFlags
 import com.infomaniak.mail.data.models.getMessages.NewMessagesResult
 import com.infomaniak.mail.data.models.mailbox.Mailbox
 import com.infomaniak.mail.data.models.mailbox.MailboxExternalMailInfo
+import com.infomaniak.mail.data.models.mailbox.MailboxHostingStatus
 import com.infomaniak.mail.data.models.mailbox.MailboxLinkedResult
 import com.infomaniak.mail.data.models.mailbox.MailboxPermissions
 import com.infomaniak.mail.data.models.mailbox.SendersRestrictions
@@ -157,7 +158,7 @@ object ApiRepository : ApiRepositoryCore() {
         return callApi(ApiRoutes.requestMailboxPassword(mailboxHostingId, mailboxName), POST)
     }
 
-    suspend fun isInfomaniakMailbox(emails: List<String>): ApiResponse<List<MailboxHostingStatus>> {
+    suspend fun isInfomaniakMailboxes(emails: List<String>): ApiResponse<List<MailboxHostingStatus>> {
         return callApi(ApiRoutes.isInfomaniakMailboxes(emails), GET)
     }
 
