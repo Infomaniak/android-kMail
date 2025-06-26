8b457fa3cf1f79087462760dd20ae96260e0dd4e
Fabian DEVEL <fabian.devel@infomaniak.com>
2025-06-26 14:28:25 +0200

    feat(EncryptionBlockSend): Block the sending if the encryption is not correctly enabled
    Block if encryption is activated with unencryptable recipient but no password

--- app/src/main/java/com/infomaniak/mail/ui/newMessage/NewMessageRecipientFieldsManager.kt
+++ app/src/main/java/com/infomaniak/mail/ui/newMessage/NewMessageRecipientFieldsManager.kt
@@ -1,6 +1,6 @@
 /*
  * Infomaniak Mail - Android
- * Copyright (C) 2023-2024 Infomaniak Network SA
+ * Copyright (C) 2023-2025 Infomaniak Network SA
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -93,7 +93,7 @@ class NewMessageRecipientFieldsManager @Inject constructor(private val snackbarM
 
     private fun onContactAdded(recipient: Recipient, fieldType: FieldType) {
         newMessageViewModel.addRecipientToField(recipient, fieldType)
-        if (newMessageViewModel.isEncryptionActivated.value == true) encryptionManager.addUnencryptableRecipient(recipient)
+        if (newMessageViewModel.isEncryptionActivated.value == true) encryptionManager.checkRecipientEncryptionStatus(recipient)
     }
 
     private fun onContactRemoved(recipient: Recipient, fieldType: FieldType) {
