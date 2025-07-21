2abb5178370ee9604edccbe5c18f3c1fb385fd3d
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2025-07-21 13:47:58 +0200

    feat: Use getBackgroundColorResBasedOnId defined inside of Core:Avatar

--- app/src/main/java/com/infomaniak/mail/utils/AvatarTypeUtils.kt
+++ app/src/main/java/com/infomaniak/mail/utils/AvatarTypeUtils.kt
@@ -22,8 +22,8 @@ import androidx.compose.ui.graphics.Color
 import com.infomaniak.core.avatar.AvatarColors
 import com.infomaniak.core.avatar.AvatarType
 import com.infomaniak.core.avatar.AvatarUrlData
+import com.infomaniak.core.avatar.getBackgroundColorResBasedOnId
 import com.infomaniak.core.coil.ImageLoaderProvider.simpleImageLoader
-import com.infomaniak.core.coil.getBackgroundColorResBasedOnId
 import com.infomaniak.lib.core.models.user.User
 import com.infomaniak.mail.R
 import com.infomaniak.mail.data.models.correspondent.Correspondent
