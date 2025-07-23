7c6ff1da919bf034f7d7c171beb8f854d54d3bee
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2025-07-23 08:54:55 +0200

    refactor: Move Avatar's models to their own "models" package

--- app/src/main/java/com/infomaniak/mail/utils/AvatarTypeUtils.kt
+++ app/src/main/java/com/infomaniak/mail/utils/AvatarTypeUtils.kt
@@ -19,9 +19,9 @@ package com.infomaniak.mail.utils
 
 import android.content.Context
 import androidx.compose.ui.graphics.Color
-import com.infomaniak.core.avatar.AvatarColors
-import com.infomaniak.core.avatar.AvatarType
-import com.infomaniak.core.avatar.AvatarUrlData
+import com.infomaniak.core.avatar.models.AvatarColors
+import com.infomaniak.core.avatar.models.AvatarType
+import com.infomaniak.core.avatar.models.AvatarUrlData
 import com.infomaniak.core.avatar.getBackgroundColorResBasedOnId
 import com.infomaniak.core.coil.ImageLoaderProvider.simpleImageLoader
 import com.infomaniak.lib.core.models.user.User
