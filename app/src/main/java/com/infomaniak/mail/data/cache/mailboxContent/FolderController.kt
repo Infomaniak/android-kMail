e46d300d8f66725c1f05fd6f55712bb7abcbd4f8
Kylian Grandy <kylian.grandy@infomaniak.com>
2025-07-28 14:33:36 +0200

    refactor: Clean code

--- app/src/main/java/com/infomaniak/mail/data/cache/mailboxContent/FolderController.kt
+++ app/src/main/java/com/infomaniak/mail/data/cache/mailboxContent/FolderController.kt
@@ -173,21 +173,19 @@ class FolderController @Inject constructor(
             withoutChildren: Boolean = false,
             visibleFoldersOnly: Boolean = true,
         ): RealmQuery<Folder> {
-            var rootsQuery = if (withoutChildren) " AND $isRootFolder" else ""
+            val rootsQuery = if (withoutChildren) " AND $isRootFolder" else ""
             val typeQuery = withoutTypes.joinToString(separator = "") {
                 when (it) {
                     FoldersType.DEFAULT -> " AND ${Folder.rolePropertyName} == nil"
-                    FoldersType.CUSTOM -> {
-                        rootsQuery = ""
-                        " AND ${Folder.rolePropertyName} != nil"
-                    }
+                    FoldersType.CUSTOM -> " AND ${Folder.rolePropertyName} != nil"
                     FoldersType.SNOOZED -> " AND ${Folder.rolePropertyName} != '${FolderRole.SNOOZED.name}'"
                     FoldersType.SCHEDULED_DRAFTS -> " AND ${Folder.rolePropertyName} != '${FolderRole.SCHEDULED_DRAFTS.name}'"
                     FoldersType.DRAFT -> " AND ${Folder.rolePropertyName} != '${FolderRole.DRAFT.name}'"
                 }
             }
             val visibilityQuery = if (visibleFoldersOnly) " AND ${Folder::isDisplayed.name} == true" else ""
-            return realm.query<Folder>("${isNotSearch}${rootsQuery}${typeQuery}${visibilityQuery}").sortFolders()
+            return realm.query<Folder>("${isNotSearch}${if (FoldersType.DEFAULT in withoutTypes) rootsQuery else ""}${typeQuery}${visibilityQuery}")
+                .sortFolders()
         }
 
         private fun getFoldersQuery(exceptionsFoldersIds: List<String>, realm: TypedRealm): RealmQuery<Folder> {
