df2da6b6c67eb62bc74b062a30a836439e162141
Vincent TE <vincent.cheun.ang.te@gmail.com>
2025-08-14 15:14:43 +0200

    chore: Update build.gradle.kts and settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,35 +11,39 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven { url 'https://jitpack.io' }
+        maven { url = uri("https://jitpack.io") }
     }
     versionCatalogs {
-        create("core") { from(files("Core/gradle/core.versions.toml")) }
+        create("core") {
+            from(files("Core/gradle/core.versions.toml"))
+        }
     }
 }
 
-rootProject.name = 'Infomaniak Mail'
-include ':app',
-        ':Core:AppIntegrity',
-        ':Core:Auth',
-        ':Core:Avatar',
-        ':Core:Coil',
-        ':Core:Compose:Basics',
-        ':Core:Compose:Margin',
-        ':Core:Compose:MaterialThemeFromXml',
-        ':Core:CrossAppLogin:Back',
-        ':Core:CrossAppLogin:Front',
-        ':Core:FragmentNavigation',
-        ':Core:Legacy',
-        ':Core:Legacy:AppLock',
-        ':Core:Legacy:BugTracker',
-        ':Core:Legacy:Confetti',
-        ':Core:Legacy:Stores',
-        ':Core:Matomo',
-        ':Core:kSuite:kSuitePro',
-        ':Core:kSuite:MyKSuite',
-        ':Core:Network',
-        ':Core:Network:Models',
-        ':Core:Sentry',
-        ':EmojiComponents',
-        ':HtmlCleaner'
+rootProject.name = "Infomaniak Mail"
+include(
+        ":app",
+        ":Core:AppIntegrity",
+        ":Core:Auth",
+        ":Core:Avatar",
+        ":Core:Coil",
+        ":Core:Compose:Basics",
+        ":Core:Compose:Margin",
+        ":Core:Compose:MaterialThemeFromXml",
+        ":Core:CrossAppLogin:Back",
+        ":Core:CrossAppLogin:Front",
+        ":Core:FragmentNavigation",
+        ":Core:Legacy",
+        ":Core:Legacy:AppLock",
+        ":Core:Legacy:BugTracker",
+        ":Core:Legacy:Confetti",
+        ":Core:Legacy:Stores",
+        ":Core:Matomo",
+        ":Core:kSuite:kSuitePro",
+        ":Core:kSuite:MyKSuite",
+        ":Core:Network",
+        ":Core:Network:Models",
+        ":Core:Sentry",
+        ":EmojiComponents",
+        ":HtmlCleaner"
+)
