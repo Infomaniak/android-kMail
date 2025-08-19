1edced26957df5b6f968389bd3a5c730c56b4ec3
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2025-08-19 09:28:59 +0200

    refactor: Use the shared view model

--- app/src/main/java/com/infomaniak/mail/ui/login/CrossAppLoginViewModel.kt
+++ app/src/main/java/com/infomaniak/mail/ui/login/CrossAppLoginViewModel.kt
@@ -17,28 +17,9 @@
  */
 package com.infomaniak.mail.ui.login
 
-import androidx.activity.ComponentActivity
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.infomaniak.core.crossapplogin.back.CrossAppLoginCommon
-import com.infomaniak.core.crossapplogin.back.CrossAppLoginCommon.LoginResult
-import com.infomaniak.core.crossapplogin.back.ExternalAccount
+import com.infomaniak.core.crossapplogin.back.BaseCrossAppLoginViewModel
 import com.infomaniak.mail.BuildConfig
 import kotlinx.serialization.ExperimentalSerializationApi
 
 @OptIn(ExperimentalSerializationApi::class)
-class CrossAppLoginViewModel : ViewModel() {
-    private val crossAppLoginCommon = CrossAppLoginCommon(viewModelScope, BuildConfig.APPLICATION_ID, BuildConfig.CLIENT_ID)
-
-    val availableAccounts by crossAppLoginCommon::availableAccounts
-    val skippedAccountIds by crossAppLoginCommon::skippedAccountIds
-    val selectedAccounts by crossAppLoginCommon::selectedAccounts
-
-    suspend fun activateUpdates(hostActivity: ComponentActivity): Nothing {
-        crossAppLoginCommon.activateUpdates(hostActivity)
-    }
-
-    suspend fun attemptLogin(selectedAccounts: List<ExternalAccount>): LoginResult {
-        return crossAppLoginCommon.attemptLogin(selectedAccounts)
-    }
-}
+class CrossAppLoginViewModel() : BaseCrossAppLoginViewModel(BuildConfig.APPLICATION_ID, BuildConfig.CLIENT_ID)
