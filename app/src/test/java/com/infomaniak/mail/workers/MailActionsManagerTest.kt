5fc97608d945a7b5c1a96924dba90484a99e1f32
Vincent TE <vincent.cheun.ang.te@gmail.com>
2025-08-27 16:15:43 +0200

    chore: Add test to know if attachments status change and uuid is set when uploaded

--- app/src/test/java/com/infomaniak/mail/workers/MailActionsManagerTest.kt
+++ app/src/test/java/com/infomaniak/mail/workers/MailActionsManagerTest.kt
@@ -27,6 +27,8 @@ import com.infomaniak.lib.core.models.ApiResponseStatus
 import com.infomaniak.mail.data.api.ApiRepository
 import com.infomaniak.mail.data.cache.RealmDatabase
 import com.infomaniak.mail.data.cache.mailboxContent.DraftController
+import com.infomaniak.mail.data.models.Attachment
+import com.infomaniak.mail.data.models.AttachmentUploadStatus
 import com.infomaniak.mail.data.models.correspondent.Recipient
 import com.infomaniak.mail.data.models.draft.Draft
 import com.infomaniak.mail.data.models.draft.SendDraftResult
@@ -34,6 +36,7 @@ import com.infomaniak.mail.data.models.mailbox.Mailbox
 import com.infomaniak.mail.utils.AccountUtils
 import com.infomaniak.mail.utils.LocalStorageUtils
 import com.infomaniak.mail.utils.NotificationUtils
+import com.infomaniak.mail.utils.extensions.AttachmentExt.startUpload
 import io.mockk.coEvery
 import io.mockk.every
 import io.mockk.mockk
@@ -44,6 +47,7 @@ import io.mockk.unmockkObject
 import io.mockk.unmockkStatic
 import io.realm.kotlin.Realm
 import io.realm.kotlin.ext.realmListOf
+import io.realm.kotlin.types.RealmList
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.runTest
 import okhttp3.OkHttpClient
@@ -67,79 +71,58 @@ class MailActionsManagerTest {
 
     @Before
     fun setup() {
-        mockkStatic(Log::class)
-        every { Log.d(any<String>(), any<String>()) } returns 0
-        every { Log.d(any<String>(), any<String>(), any<Throwable>()) } returns 0
-
-        every { Log.i(any<String>(), any<String>()) } returns 0
-        every { Log.i(any<String>(), any<String>(), any<Throwable>()) } returns 0
-
-        mockkObject(ApiRepository)
-        coEvery { ApiRepository.sendDraft(any(), any(), any()) } coAnswers {
-            ApiResponse(
-                result = ApiResponseStatus.SUCCESS,
-                data = SendDraftResult(scheduledMessageEtop = "2025-08-27T10:45:48+0200")
-            )
-        }
-
-        mockkObject(LocalStorageUtils)
-        val mock = spyk<LocalStorageUtils>(recordPrivateCalls = true)
-        every { mock["getDraftUploadDir"](any<Context>(), any<String>(), any<Int>(), any<Int>()) } returns File("")
-        every { LocalStorageUtils.deleteDraftUploadDir(any(), any(), any(), any(), any()) } returns Unit
+        mockLogFunctions()
+        mockApiRepository()
+        mockLocalStorageUtils()
 
         every { coroutineWorker.applicationContext } returns context
 
-        InfomaniakCore.appVersionName = "0.0.0"
-        InfomaniakCore.appId = "0"
-
-        AccountUtils.currentUserId = 0
-        AccountUtils.currentMailboxId = 0
-        AccountUtils.loadInMemory = true
-
-        mailboxContent = RealmDatabase.MailboxContent()
-        draftController = DraftController(mailboxContent!!)
+        initializeConstants()
+        initializeMailRelatedStuff()
     }
 
     @Test
-    fun `test mail actions manager`() = runTest {
+    fun `test sending email`() = runTest {
         mailboxContentRealm?.let { realm ->
             realm.write {
                 launch {
-                    val draft = Draft().apply {
-                        action = Draft.DraftAction.SEND
-                        body = "This is a test email."
-                        subject = "This is a test subject."
-                        to = realmListOf(
-                            Recipient().apply {
-                                email = "test@example.com"
-                                name = "Test"
-                            }
-                        )
-                        cc = realmListOf()
-                        bcc = realmListOf()
-                    }
-                    draftController?.upsertDraft(draft)
-
+                    draftController?.upsertDraft(getDraft())
                     assert(draftController?.getAllDrafts(realm)?.count() == 1) { "We should have one draft" }
-
-                    val mailActionsManager = MailActionsManager(
-                        mailboxContentRealm = mailboxContentRealm!!,
-                        userId = 0,
-                        mailboxId = 0,
-                        mailbox = Mailbox(),
-                        isSnackbarFeedbackNeeded = false,
-                        draftLocalUuid = "",
-                        draftController = draftController!!,
-                        okHttpClient = okHttpClient,
-                        coroutineWorker = coroutineWorker,
-                        isAppInBackground = { false },
-                        notificationUtils = notificationUtils,
-                        notificationManagerCompat = notificationManagerCompat,
-                    )
-
-                    mailActionsManager.handleDraftsActions()
-
+                    getMailActionManager(mailboxContentRealm).handleDraftsActions()
                     assert(draftController?.getAllDrafts(realm)?.count() == 0) { "Drafts should be empty" }
+                }
+            }
+        }
+    }
+
+    @Test
+    fun `test attachments`() = runTest {
+        mailboxContentRealm?.let { realm ->
+            realm.write {
+
+                launch {
+                    val file = mockk<File>()
+                    every { file.exists() } answers { true }
+
+                    val mockedAttachment = spyk(getAttachment())
+                    every { mockedAttachment.getUploadLocalFile() } answers { file }
+
+                    mockkObject(AccountUtils)
+                    coEvery { AccountUtils.getUserById(any())?.apiToken?.accessToken } answers { "token" }
+
+                    mockApiRepository()
+
+                    draftController?.upsertDraft(getDraft(realmListOf(getAttachment())))
+
+                    val storedAttachment = draftController?.getDraft(DRAFT_LOCAL_UUID)?.attachments?.first()
+                    assert(storedAttachment?.uuid?.isEmpty() == true)
+                    assert(storedAttachment?.attachmentUploadStatus == AttachmentUploadStatus.NOT_UPLOADED)
+
+                    mockedAttachment.startUpload(DRAFT_LOCAL_UUID, Mailbox(), realm)
+
+                    val updatedStoredAttachment = draftController?.getDraft(DRAFT_LOCAL_UUID)?.attachments?.first()
+                    assert(updatedStoredAttachment?.uuid == ATTACHMENT_REMOTE_UUID)
+                    assert(updatedStoredAttachment?.attachmentUploadStatus == AttachmentUploadStatus.UPLOADED)
                 }
             }
         }
@@ -150,5 +133,102 @@ class MailActionsManagerTest {
         unmockkStatic(Log::class)
         unmockkObject(ApiRepository)
         unmockkObject(LocalStorageUtils)
+        unmockkObject(AccountUtils)
+    }
+
+    private fun mockLogFunctions() {
+        mockkStatic(Log::class)
+        every { Log.d(any<String>(), any<String>()) } returns 0
+        every { Log.d(any<String>(), any<String>(), any<Throwable>()) } returns 0
+        every { Log.i(any<String>(), any<String>()) } returns 0
+        every { Log.i(any<String>(), any<String>(), any<Throwable>()) } returns 0
+    }
+
+    private fun mockApiRepository() {
+        mockkObject(ApiRepository)
+        coEvery { ApiRepository.sendDraft(any(), any(), any()) } coAnswers {
+            ApiResponse(
+                result = ApiResponseStatus.SUCCESS,
+                data = SendDraftResult(scheduledMessageEtop = "2025-08-27T10:45:48+0200")
+            )
+        }
+        coEvery { ApiRepository.createAttachments(any(), any(), any(), any()) } coAnswers {
+            ApiResponse(
+                result = ApiResponseStatus.SUCCESS,
+                data = getAttachment(remoteUUID = ATTACHMENT_REMOTE_UUID)
+            )
+        }
+    }
+
+    private fun mockLocalStorageUtils() {
+        mockkObject(LocalStorageUtils)
+        val mock = spyk<LocalStorageUtils>(recordPrivateCalls = true)
+        every { mock["getDraftUploadDir"](any<Context>(), any<String>(), any<Int>(), any<Int>()) } returns File("")
+        every { LocalStorageUtils.deleteDraftUploadDir(any(), any(), any(), any(), any()) } returns Unit
+    }
+
+    private fun initializeConstants() {
+        InfomaniakCore.appVersionName = "0.0.0"
+        InfomaniakCore.appId = "0"
+
+        AccountUtils.currentUserId = 0
+        AccountUtils.currentMailboxId = 0
+        AccountUtils.loadInMemory = true
+    }
+
+    private fun initializeMailRelatedStuff() {
+        mailboxContent = RealmDatabase.MailboxContent()
+        draftController = DraftController(mailboxContent!!)
+    }
+
+    private fun getMailActionManager(mailboxContentRealm: Realm?): MailActionsManager {
+        return MailActionsManager(
+            mailboxContentRealm = mailboxContentRealm!!,
+            userId = 0,
+            mailboxId = 0,
+            mailbox = Mailbox(),
+            isSnackbarFeedbackNeeded = false,
+            draftLocalUuid = "",
+            draftController = draftController!!,
+            okHttpClient = okHttpClient,
+            coroutineWorker = coroutineWorker,
+            isAppInBackground = { false },
+            notificationUtils = notificationUtils,
+            notificationManagerCompat = notificationManagerCompat,
+        )
+    }
+
+    private fun getDraft(attachments: RealmList<Attachment> = realmListOf()): Draft {
+        return Draft().apply {
+            localUuid = DRAFT_LOCAL_UUID
+            action = Draft.DraftAction.SEND
+            body = "This is a test email."
+            subject = "This is a test subject."
+            to = realmListOf(
+                Recipient().apply {
+                    email = "test@example.com"
+                    name = "Test"
+                }
+            )
+            cc = realmListOf()
+            bcc = realmListOf()
+            this.attachments = attachments
+        }
+    }
+
+    private fun getAttachment(remoteUUID: String = ""): Attachment = Attachment().initLocalValues(
+        name = "attachment",
+        size = 1024L,
+        mimeType = "image/png",
+        uri = "file://path/to/attachment",
+    ).apply {
+        localUuid = ATTACHMENT_LOCAL_UUID
+        uuid = remoteUUID
+    }
+
+    companion object {
+        private const val DRAFT_LOCAL_UUID = "draft-local-uuid"
+        private const val ATTACHMENT_LOCAL_UUID = "attachment-local-uuid"
+        private const val ATTACHMENT_REMOTE_UUID = "attachment-remote-uuid"
     }
 }
