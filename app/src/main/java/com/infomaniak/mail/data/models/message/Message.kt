d645270f984ee17fef3fc3a4cf8a7ff8bf552933
Jamy Bailly <jamy.bailly@infomaniak.com>
2025-09-17 15:02:58 +0200

    refactor: Clarify logic of notification preview

--- app/src/main/java/com/infomaniak/mail/data/models/message/Message.kt
+++ app/src/main/java/com/infomaniak/mail/data/models/message/Message.kt
@@ -416,11 +416,18 @@ class Message : RealmObject, Snoozable {
         snoozeEndDate = flags.snoozeEndDate.toRealmInstant()
     }
 
-    fun getFormattedPreview(context: Context): String = "\n" + when {
-        isEncrypted -> context.getString(R.string.encryptedMessageHeader)
-        isReaction -> context.getString(R.string.previewReaction, from.first().name, emojiReaction)
-        preview.isBlank() -> context.getString(R.string.noBodyDescription)
-        else -> preview.trim()
+    fun getFormattedPreview(context: Context, previewType: PreviewType): String = "\n" + when (previewType) {
+        PreviewType.Encryption -> context.getString(R.string.encryptedMessageHeader)
+        PreviewType.Reaction -> context.getString(R.string.previewReaction, from.first().name, emojiReaction)
+        PreviewType.Empty -> context.getString(R.string.noBodyDescription)
+        PreviewType.Body -> preview.trim()
+    }
+
+    fun getPreviewType(): PreviewType = when {
+        isEncrypted -> PreviewType.Encryption
+        isReaction -> PreviewType.Reaction
+        preview.isBlank() -> PreviewType.Empty
+        else -> PreviewType.Body
     }
 
     fun shouldBeExpanded(index: Int, lastIndex: Int) = !isDraft && (!isSeen || index == lastIndex)
@@ -441,6 +448,13 @@ class Message : RealmObject, Snoozable {
     override fun equals(other: Any?) = other === this || (other is Message && other.uid == uid)
 
     override fun hashCode(): Int = uid.hashCode()
+
+    enum class PreviewType {
+        Encryption,
+        Reaction,
+        Empty,
+        Body
+    }
 
     companion object {
         // Encountered formats so far:
