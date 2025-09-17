2b33c263076256c27d3fb65286d50f79a0576884
Kevin Boulongne <kevin.boulongne@infomaniak.com>
2025-09-17 10:08:10 +0200

    fix: When removing unused variables in Message, we forgot to migrate Realm

--- app/src/main/java/com/infomaniak/mail/data/cache/RealmDatabase.kt
+++ app/src/main/java/com/infomaniak/mail/data/cache/RealmDatabase.kt
@@ -198,7 +198,7 @@ object RealmDatabase {
         //region Configurations versions
         const val USER_INFO_SCHEMA_VERSION = 5L
         const val MAILBOX_INFO_SCHEMA_VERSION = 10L
-        const val MAILBOX_CONTENT_SCHEMA_VERSION = 33L
+        const val MAILBOX_CONTENT_SCHEMA_VERSION = 34L
         //endregion
 
         //region Configurations names
