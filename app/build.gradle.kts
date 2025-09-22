9af160a5f23f8d826d900f48fd3fdf37a2c2f233
Vincent TE <vincent.cheun.ang.te@gmail.com>
2025-09-22 17:47:37 +0200

    chore: Display an error if ui-test.env.properties is missing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,9 +68,14 @@ android {
         buildConfigField("String", "GITHUB_REPO", "\"android-mail\"")
         buildConfigField("String", "GITHUB_REPO_URL", "\"https://github.com/Infomaniak/android-kMail\"")
 
+        //noinspection WrongGradleMethod
+        val isRelease = gradle.startParameter.taskNames.any { it.contains("release", ignoreCase = true) }
+
         val uiTestEnvProperties = rootProject.file("uitest-env.properties").takeIf { it.exists() }?.let { file ->
             Properties().also { it.load(file.reader()) }
         }
+
+        if (!isRelease && uiTestEnvProperties == null) error("The `uitest-env.properties` file must exist (see `uitest-env.example.properties`).")
 
         val uiTestAccountEmail = uiTestEnvProperties?.getProperty("uiTestAccountEmail").takeUnless { it.isNullOrBlank() }
         val uiTestAccountPassword = uiTestEnvProperties?.getProperty("uiTestAccountPassword").takeUnless { it.isNullOrBlank() }
