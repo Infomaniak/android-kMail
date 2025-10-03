d3134a8d4268e693e299df8454d1d2184f16f5b3
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2025-10-03 15:59:00 +0200

    refactor: Move flattenFolderChildren in FolderController which is the only file using it

--- app/src/main/java/com/infomaniak/mail/data/cache/mailboxContent/FolderController.kt
+++ app/src/main/java/com/infomaniak/mail/data/cache/mailboxContent/FolderController.kt
@@ -26,8 +26,8 @@ import com.infomaniak.mail.data.models.mailbox.Mailbox
 import com.infomaniak.mail.data.models.thread.Thread
 import com.infomaniak.mail.utils.extensions.copyListToRealm
 import com.infomaniak.mail.utils.extensions.findSuspend
-import com.infomaniak.mail.utils.extensions.flattenFolderChildrenAndRemoveMessages
 import com.infomaniak.mail.utils.extensions.sortFolders
+import com.infomaniak.mail.utils.shouldBeExcluded
 import io.realm.kotlin.MutableRealm
 import io.realm.kotlin.Realm
 import io.realm.kotlin.TypedRealm
@@ -79,7 +79,7 @@ class FolderController @Inject constructor(
 
     //region Edit data
     suspend fun update(mailbox: Mailbox, remoteFolders: List<Folder>, realm: Realm) {
-        val remoteFoldersWithChildren = remoteFolders.flattenFolderChildrenAndRemoveMessages()
+        val remoteFoldersWithChildren = remoteFolders.flattenFolderChildren()
 
         realm.write {
 
@@ -223,5 +223,44 @@ class FolderController @Inject constructor(
             }
         }
         //endregion
+    }
+}
+
+private fun List<Folder>.flattenFolderChildren(): List<Folder> {
+    return if (isEmpty()) this else formatFolderWithAllChildren(toMutableList())
+}
+
+private tailrec fun formatFolderWithAllChildren(
+    inputList: MutableList<Folder>,
+    outputList: MutableList<Folder> = mutableListOf(),
+): List<Folder> {
+
+    val folder = inputList.removeAt(0)
+
+    /*
+    * There are two types of folders:
+    * - user's folders (with or without a role)
+    * - hidden IK folders (ScheduledDrafts, Snoozed, etc…)
+    *
+    * We want to display the user's folders, and also the IK folders for which we handle the role.
+    * IK folders where we don't handle the role are dismissed.
+    *
+    * I.e. hides all IK folders with no roles.
+    */
+    val shouldThisFolderBeAdded = folder.shouldBeExcluded(excludeRoleFolder = false).not()
+
+    val children = if (shouldThisFolderBeAdded) {
+        outputList.add(folder)
+        folder.children.sortFolders()
+    } else {
+        null
+    }
+
+    children?.let { inputList.addAll(index = 0, it) }
+
+    return if (inputList.isEmpty()) {
+        outputList
+    } else {
+        formatFolderWithAllChildren(inputList, outputList)
     }
 }
