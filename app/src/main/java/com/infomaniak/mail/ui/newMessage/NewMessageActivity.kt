889a42ab0e41e4a5099886a105286f3bc6a5e18c
Jamy Bailly <jamy.bailly@infomaniak.com>
2025-10-06 10:59:26 +0200

    feat: Open SelectMailbox on mailto

--- app/src/main/java/com/infomaniak/mail/ui/newMessage/NewMessageActivity.kt
+++ app/src/main/java/com/infomaniak/mail/ui/newMessage/NewMessageActivity.kt
@@ -78,6 +78,21 @@ class NewMessageActivity : BaseActivity() {
         ShortcutManagerCompat.reportShortcutUsed(this@NewMessageActivity, Shortcuts.NEW_MESSAGE.id)
         setContentView(binding.root)
 
+        val navGraph = navController.navInflater.inflate(R.navigation.new_message_navigation)
+        when (intent.action) {
+            Intent.ACTION_SEND,
+            Intent.ACTION_SEND_MULTIPLE,
+            Intent.ACTION_VIEW,
+            Intent.ACTION_SENDTO -> {
+                navGraph.setStartDestination(R.id.selectMailboxFragment)
+                navController.graph = navGraph
+            }
+            else -> {
+                navGraph.setStartDestination(R.id.newMessageFragment)
+            }
+        }
+        navController.graph = navGraph
+
         if (!isAuth()) {
             finish()
             return
