14eee6f516546ed6c19743907793777b669fa692
Jamy Bailly <jamy.bailly@infomaniak.com>
2025-10-07 11:13:13 +0200

    fix: Rename feature package

--- app/src/main/java/com/infomaniak/mail/ui/newMessage/selectMailbox/compose/previewparameter/SelectedMailboxPreviewParameter.kt
+++ app/src/main/java/com/infomaniak/mail/ui/newMessage/selectMailbox/compose/previewparameter/SelectedMailboxPreviewParameter.kt
@@ -15,11 +15,11 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package com.infomaniak.mail.ui.newMessage.mailbox.compose.previewparameter
+package com.infomaniak.mail.ui.newMessage.selectMailbox.compose.previewparameter
 
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
-import com.infomaniak.mail.ui.newMessage.mailbox.SelectMailboxViewModel.MailboxUi
-import com.infomaniak.mail.ui.newMessage.mailbox.SelectMailboxViewModel.SelectedMailboxUi
+import com.infomaniak.mail.ui.newMessage.selectMailbox.SelectMailboxViewModel.MailboxUi
+import com.infomaniak.mail.ui.newMessage.selectMailbox.SelectMailboxViewModel.SelectedMailboxUi
 
 class SelectedMailboxPreviewParameter : PreviewParameterProvider<SelectedMailboxUi> {
     override val values: Sequence<SelectedMailboxUi>
