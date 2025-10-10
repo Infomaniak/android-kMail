Infomaniak/android-kMail

commit 05a0bd52ceda110e749c934c476b51357dc0de43

Extract previous message fetching from `setPreviousMessage()` for clarity and logic

app/src/main/java/com/infomaniak/mail/ui/newMessage/NewMessageViewModel.kt
@@ -348,26 +348,22 @@ class NewMessageViewModel @Inject constructor(
             DraftMode.REPLY, DraftMode.REPLY_ALL, DraftMode.FORWARD -> {
                 previousMessageUid
                     ?.let { uid -> MessageController.getMessage(uid, realm) }
-                    ?.let { message ->
-                        val (isSuccess, previousMessageBody) = draftController.setPreviousMessage(
-                            draft = this,
-                            draftMode = draftMode,
-                            message = message,
-                            realm = realm,
-                        )
-                        if (!isSuccess) return null
+                    ?.let { previousMessage ->
+                        val (fullMessage, hasFailedFetching) = draftController.fetchHeavyDataIfNeeded(previousMessage, realm)
 
+                        if (hasFailedFetching) return null
 
-                        if (currentMailbox.featureFlags.contains(FeatureFlag.AI)) {
-                            parsePreviousMailForAnsweringWithAi(previousMessageBody)
-                        }
-                        if (shouldPreselectSignature) preSelectSignature(message, signatures)
+                        draftController.setPreviousMessage(draft = this, draftMode = draftMode, previousMessage = fullMessage)
+
+                        val isAiEnabled = currentMailbox.featureFlags.contains(FeatureFlag.AI)
+                        if (isAiEnabled) parsePreviousMailToAnswerWithAi(fullMessage.body!!)
+                        if (shouldPreselectSignature) preSelectSignature(previousMessage, signatures)
                     }
             }
         }
     }
 
-    private suspend fun parsePreviousMailForAnsweringWithAi(previousMessageBody: Body) {
+    private suspend fun parsePreviousMailToAnswerWithAi(previousMessageBody: Body) {
         if (draftMode == DraftMode.REPLY || draftMode == DraftMode.REPLY_ALL) {
             previousMessageBodyPlainText = previousMessageBody.asPlainText()
         }

app/src/main/java/com/infomaniak/mail/data/cache/mailboxContent/DraftController.kt
@@ -27,7 +27,6 @@ import com.infomaniak.mail.data.models.Attachment
 import com.infomaniak.mail.data.models.correspondent.Recipient
 import com.infomaniak.mail.data.models.draft.Draft
 import com.infomaniak.mail.data.models.draft.Draft.DraftMode
-import com.infomaniak.mail.data.models.message.Body
 import com.infomaniak.mail.data.models.message.Message
 import com.infomaniak.mail.ui.main.thread.MessageWebViewClient.Companion.CID_SCHEME
 import com.infomaniak.mail.utils.AccountUtils
@@ -85,18 +84,7 @@ class DraftController @Inject constructor(
     //endregion
 
     //region Open Draft
-    fun setPreviousMessage(draft: Draft, draftMode: DraftMode, message: Message, realm: Realm): Pair<Boolean, Body> {
-
-        var isSuccess = true
-
-        val previousMessage = if (message.isFullyDownloaded()) {
-            message
-        } else {
-            val (deleted, failed) = ThreadController.fetchMessagesHeavyData(listOf(message), realm)
-            isSuccess = deleted.isEmpty() && failed.isEmpty()
-            MessageController.getMessage(message.uid, realm)!!
-        }
-
+    fun setPreviousMessage(draft: Draft, draftMode: DraftMode, previousMessage: Message) {
         draft.inReplyTo = previousMessage.messageId
 
         val previousReferences = if (previousMessage.references == null) "" else "${previousMessage.references} "
@@ -128,8 +116,14 @@ class DraftController @Inject constructor(
             }
             DraftMode.NEW_MAIL -> Unit
         }
+    }
 
-        return isSuccess to previousMessage.body!!
+    fun fetchHeavyDataIfNeeded(message: Message, realm: Realm): Pair<Message, Boolean> {
+        if (message.isFullyDownloaded()) return message to false
+
+        val (deleted, failed) = ThreadController.fetchMessagesHeavyData(listOf(message), realm)
+        val hasFailedFetching = deleted.isNotEmpty() || failed.isNotEmpty()
+        return MessageController.getMessage(message.uid, realm)!! to hasFailedFetching
     }
 
     private fun Context.replyQuote(message: Message): String {

commit 3eddc31804187938a3bf5b117e993574bebbdec8

fix: Fix rare NullPointerException + log it to sentry
This fixes the regression introduced in commit 05a0bd52

app/src/main/java/com/infomaniak/mail/ui/newMessage/NewMessageViewModel.kt
@@ -105,6 +105,7 @@ import io.realm.kotlin.ext.copyFromRealm
 import io.realm.kotlin.ext.realmListOf
 import io.realm.kotlin.ext.toRealmList
 import io.realm.kotlin.types.RealmList
+import io.sentry.Breadcrumb
 import io.sentry.Sentry
 import kotlinx.coroutines.CompletableJob
 import kotlinx.coroutines.CoroutineDispatcher
@@ -363,8 +364,13 @@ class NewMessageViewModel @Inject constructor(
         val quote = draftInitManager.createQuote(draftMode, fullMessage, draft.attachments)
         if (quote != null) initialQuote = quote
 
+        if (fullMessage.body == null) {
+            SentryLog.e(TAG, "The message we're trying to reply to has an unexpected null body") { scope ->
+                scope.addBreadcrumb(Breadcrumb.info("message resource: ${fullMessage.resource}"))
+            }
+        }
         val isAiEnabled = currentMailbox().featureFlags.contains(FeatureFlag.AI)
-        if (isAiEnabled) parsePreviousMailToAnswerWithAi(fullMessage.body!!)
+        if (isAiEnabled) parsePreviousMailToAnswerWithAi(fullMessage.body)
 
         val isEncryptionEnabled = currentMailbox().featureFlags.contains(FeatureFlag.ENCRYPTION)
         if (isEncryptionEnabled) draft.isEncrypted = fullMessage.isEncrypted
@@ -567,9 +573,9 @@ class NewMessageViewModel @Inject constructor(
         }
     }
 
-    private suspend fun parsePreviousMailToAnswerWithAi(previousMessageBody: Body) {
+    private suspend fun parsePreviousMailToAnswerWithAi(previousMessageBody: Body?) {
         if (draftMode == DraftMode.REPLY || draftMode == DraftMode.REPLY_ALL) {
-            aiSharedData.previousMessageBodyPlainText = previousMessageBody.asPlainText()
+            aiSharedData.previousMessageBodyPlainText = previousMessageBody?.asPlainText()
         }
     }
 