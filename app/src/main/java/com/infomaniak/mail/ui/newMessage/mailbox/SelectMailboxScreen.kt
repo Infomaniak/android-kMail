0bdaf0bb0272b27ef3f9a7d30aa6a48baa6c8ee6
Jamy Bailly <jamy.bailly@infomaniak.com>
2025-10-16 08:44:44 +0200

    fix: Remove useless AnimatedVisibility

--- app/src/main/java/com/infomaniak/mail/ui/newMessage/mailbox/SelectMailboxScreen.kt
+++ app/src/main/java/com/infomaniak/mail/ui/newMessage/mailbox/SelectMailboxScreen.kt
@@ -18,7 +18,6 @@
 package com.infomaniak.mail.ui.newMessage.mailbox
 
 import android.content.res.Configuration
-import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -123,13 +122,11 @@ fun SelectMailboxScreen(
                     text = stringResource(R.string.composeMailboxCurrentTitle)
                 )
                 if (!selectingAnotherUser.value) {
-                    AnimatedVisibility(!selectingAnotherUser.value) {
-                        selectedMailbox?.let {
-                            SelectedMailboxIndicator(
-                                modifier = Modifier.padding(Margin.Medium),
-                                selectedMailbox = it
-                            )
-                        }
+                    selectedMailbox?.let {
+                        SelectedMailboxIndicator(
+                            modifier = Modifier.padding(Margin.Medium),
+                            selectedMailbox = it
+                        )
                     }
                 } else {
                     Column {
