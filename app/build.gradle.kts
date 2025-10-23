24222dac8983bad91b9bf486a44584e2d9e6a2bc
Louis CAD <louis.cognault@infomaniak.com>
2025-10-23 19:08:08 +0200

    chore(Version): 1.20.0-3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,7 +31,7 @@ android {
         applicationId = "com.infomaniak.mail"
         minSdk = appMinSdk
         targetSdk = appCompileSdk
-        versionCode = 1_20_000_02
+        versionCode = 1_20_000_03
         versionName = "1.20.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         testInstrumentationRunnerArguments["clearPackageData"] = "true"
