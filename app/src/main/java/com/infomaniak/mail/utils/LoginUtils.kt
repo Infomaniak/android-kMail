7247792cce3185b8e6c92ac40b99eae2aecf79ed
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2025-11-26 15:12:04 +0100

    chore: Remove useless Dispatchers.IO

--- app/src/main/java/com/infomaniak/mail/utils/LoginUtils.kt
+++ app/src/main/java/com/infomaniak/mail/utils/LoginUtils.kt
@@ -47,7 +47,6 @@ import com.infomaniak.mail.utils.extensions.launchNoMailboxActivity
 import com.infomaniak.mail.utils.extensions.launchNoValidMailboxesActivity
 import dagger.hilt.android.scopes.ActivityScoped
 import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.invoke
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
@@ -102,7 +101,7 @@ class LoginUtils @Inject constructor(
             chain.proceed(newRequest)
         }.build()
 
-        val apiResponse = Dispatchers.IO { ApiRepository.getMailboxes(okhttpClient) }
+        val apiResponse = ApiRepository.getMailboxes(okhttpClient)
 
         return when {
             !apiResponse.isSuccess() -> apiResponse
