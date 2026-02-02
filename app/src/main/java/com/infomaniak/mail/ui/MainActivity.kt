ff7495f01e8b18ac0192d0f4c003b19410863c01
Sol Rubado <sol.rubado@infomaniak.com>
2026-02-02 08:46:09 +0100

    fix: Permanently delete trigger and multiselect phishing

--- app/src/main/java/com/infomaniak/mail/ui/MainActivity.kt
+++ app/src/main/java/com/infomaniak/mail/ui/MainActivity.kt
@@ -273,7 +273,7 @@ class MainActivity : BaseActivity() {
     }
 
     private fun observeActivityDialogLoaderReset() {
-        mainViewModel.activityDialogLoaderResetTrigger.observe(this) { descriptionDialog.resetLoadingAndDismiss() }
+        actionsViewModel.activityDialogLoaderResetTrigger.observe(this) { descriptionDialog.resetLoadingAndDismiss() }
     }
 
     private fun observeDraftWorkerResults() {
