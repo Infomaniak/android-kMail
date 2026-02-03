254a084abf4ceb3f25a145ec809e5581b60e5f9f
Sol Rubado <sol.rubado@infomaniak.com>
2026-02-03 16:14:40 +0100

    feat: Add signature to rich editor

--- app/src/main/java/com/infomaniak/mail/utils/MessageBodyUtils.kt
+++ app/src/main/java/com/infomaniak/mail/utils/MessageBodyUtils.kt
@@ -35,6 +35,7 @@ object MessageBodyUtils {
     const val INFOMANIAK_SIGNATURE_HTML_CLASS_NAME = "editorUserSignature"
     const val INFOMANIAK_REPLY_QUOTE_HTML_CLASS_NAME = "ik_mail_quote"
     const val INFOMANIAK_FORWARD_QUOTE_HTML_CLASS_NAME = "forwardContentMessage"
+    const val INFOMANIAK_SIGNATURE_HTML_ID = "ik-signature"
 
     private const val QUOTE_DETECTION_TIMEOUT = 1_500L
 
