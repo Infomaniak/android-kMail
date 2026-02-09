e0e9421baec90d22662eec43db1157a10cb1bd33
Fabian DEVEL <fabian.devel@infomaniak.com>
2026-02-09 16:27:39 +0100

    chore(Version): 1.24.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "com.infomaniak.mail"
         minSdk = appMinSdk
         targetSdk = appCompileSdk
-        versionCode = 1_24_002_01
-        versionName = "1.24.2"
+        versionCode = 1_24_003_01
+        versionName = "1.24.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         testInstrumentationRunnerArguments["clearPackageData"] = "true"
         testInstrumentationRunnerArguments["useTestStorageService"] = "true"
