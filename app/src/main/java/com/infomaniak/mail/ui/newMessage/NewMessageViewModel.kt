4d59bb03f2a486b1b09caed0aaa7317016c348f3
Sol Rubado <sol.rubado@infomaniak.com>
2026-02-16 14:29:34 +0100

    refactor: Remove unnecesary log

--- app/src/main/java/com/infomaniak/mail/ui/newMessage/NewMessageViewModel.kt
+++ app/src/main/java/com/infomaniak/mail/ui/newMessage/NewMessageViewModel.kt
@@ -24,7 +24,6 @@ import android.content.ClipDescription
 import android.content.Intent
 import android.net.Uri
 import android.os.Parcelable
-import android.util.Log
 import androidx.core.app.NotificationManagerCompat
 import androidx.core.net.MailTo
 import androidx.core.net.toUri
@@ -606,7 +605,6 @@ class NewMessageViewModel @Inject constructor(
     }
 
     fun bodyHasQuotes(bodyHtml: String): Boolean {
-        Log.d("SIGNATURE HTML", bodyHtml)
         return JsoupParserUtil.jsoupParseBodyFragmentWithLog(bodyHtml).getElementById(INFOMANIAK_QUOTES_HTML_ID) != null
     }
 
