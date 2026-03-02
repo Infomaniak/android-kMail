80e5a482d73e9e4ef3b9b7c16312411d75eb061c
Sol Rubado <sol.rubado@infomaniak.com>
2026-03-02 14:54:14 +0100

    feat: Change 3 dots for text button and fix ai PR suggestions

---
+++ app/src/main/res/raw/replace_signature_script.js
@@ -0,0 +1,36 @@
+/*
+ * Infomaniak Mail - Android
+ * Copyright (C) 2026 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+(function() {
+    var signatureElement = document.querySelector('.%s');
+    var newSigHtml = "%s";
+    if (signatureElement) {
+        if (newSigHtml === "") {
+            signatureElement.remove();
+        } else {
+            signatureElement.outerHTML = newSigHtml;
+        }
+    } else if (newSigHtml !== "") {
+        var quotes = document.querySelector('.%s');
+        if (quotes) {
+            quotes.insertAdjacentHTML('beforebegin', newSigHtml);
+        } else {
+            document.body.insertAdjacentHTML('beforeend', newSigHtml);
+        }
+    }
+})()
