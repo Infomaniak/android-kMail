690334bce84910c5ab837e29a836db9f19790f53
Sol Rubado <sol.rubado@infomaniak.com>
2026-03-17 16:37:47 +0100

    fix: Fix sonar issue

--- app/src/main/res/raw/editor_javascript_bridge.js
+++ app/src/main/res/raw/editor_javascript_bridge.js
@@ -16,5 +16,5 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
  function onImagesDeletedFromQuotes(cids) {
-     window.kmail.onImagesDeletedFromQuotes(cids);
+     globalThis.kmail.onImagesDeletedFromQuotes(cids);
  }
