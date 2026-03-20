d2f6ab73c697b61084739958620bd1b9d78f5ac3
Sol Rubado <sol.rubado@infomaniak.com>
2026-03-20 14:11:53 +0100

    fix: Put refresh threads in viewModels and create download threads status manager

--- app/src/main/java/com/infomaniak/mail/ui/main/folder/ThreadListFragment.kt
+++ app/src/main/java/com/infomaniak/mail/ui/main/folder/ThreadListFragment.kt
@@ -39,13 +39,13 @@ import androidx.fragment.app.FragmentContainerView
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle.State
-import androidx.lifecycle.distinctUntilChanged
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView.Adapter.StateRestorationPolicy
 import com.infomaniak.core.common.extensions.goToAppStore
+import com.infomaniak.core.common.observe
 import com.infomaniak.core.common.utils.isToday
 import com.infomaniak.core.inappupdate.updatemanagers.InAppUpdateManager
 import com.infomaniak.core.ksuite.data.KSuite
@@ -95,6 +95,7 @@ import com.infomaniak.mail.ui.main.thread.actions.EmojiReactionsViewModel
 import com.infomaniak.mail.ui.main.user.SwitchUserViewModel
 import com.infomaniak.mail.ui.newMessage.NewMessageActivityArgs
 import com.infomaniak.mail.utils.AccountUtils
+import com.infomaniak.mail.utils.DownloadThreadsStatusManager
 import com.infomaniak.mail.utils.FolderRoleUtils
 import com.infomaniak.mail.utils.PlayServicesUtils
 import com.infomaniak.mail.utils.RealmChangesBinding.Companion.bindResultsChangeToAdapter
@@ -148,6 +149,9 @@ class ThreadListFragment : TwoPaneFragment(), PickerEmojiObserver {
 
     @Inject
     lateinit var descriptionDialog: DescriptionAlertDialog
+
+    @Inject
+    lateinit var downloadThreadsStatusManager: DownloadThreadsStatusManager
 
     @Inject
     lateinit var folderRoleUtils: FolderRoleUtils
@@ -347,7 +351,7 @@ class ThreadListFragment : TwoPaneFragment(), PickerEmojiObserver {
 
     private fun setupOnRefresh() {
         binding.swipeRefreshLayout.setOnRefreshListener {
-            if (mainViewModel.isDownloadingChanges.value == true) return@setOnRefreshListener
+            if (downloadThreadsStatusManager.isDownloading.value) return@setOnRefreshListener
             mainViewModel.forceRefreshThreads()
         }
     }
@@ -666,8 +670,7 @@ class ThreadListFragment : TwoPaneFragment(), PickerEmojiObserver {
     }
 
     private fun observeDownloadState() {
-        mainViewModel.isDownloadingChanges
-            .distinctUntilChanged()
+        downloadThreadsStatusManager.isDownloading
             .observe(viewLifecycleOwner) { isDownloading ->
                 if (isDownloading) {
                     showLoadingTimer.start()
