11557ff0648094601f233215721a3863e6e1769c
Aymeric MARIAUX <aymeric.mariaux@infomaniak.com>
2026-04-22 08:23:18 +0200

    fix: Remove unnecessary computation

--- app/src/main/java/com/infomaniak/mail/ui/newMessage/NewMessageViewModel.kt
+++ app/src/main/java/com/infomaniak/mail/ui/newMessage/NewMessageViewModel.kt
@@ -265,9 +265,7 @@ class NewMessageViewModel @Inject constructor(
 
     fun shouldShowAttachmentReminder(uiBodyValue: String): Boolean {
         val hasAttachments = attachmentsLiveData.valueOrEmpty().isNotEmpty()
-        val plainText = jsoupParseWithLog(uiBodyValue).text()
-
-        return AttachmentReminderUtils.hasAttachmentKeyword(plainText) && !hasAttachments
+        return !hasAttachments && AttachmentReminderUtils.hasAttachmentKeyword(uiBodyValue)
     }
 
     // ------------- !IMPORTANT! -------------
