992ce2d76f58c1f8f55ea3032a7db3da119d04a4
Elouan BOITEUX <elouan.boiteux@infomaniak.com>
2026-04-30 16:48:29 +0200

    feat: Create reusable function to handle both disabled states with and without actions

---
+++ app/src/main/java/com/infomaniak/mail/utils/extensions/ViewExt.kt
@@ -0,0 +1,85 @@
+/*
+ * Infomaniak Mail - Android
+ * Copyright (C) 2026 Infomaniak Network SA
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.infomaniak.mail.utils.extensions
+
+import android.content.res.ColorStateList
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.button.MaterialButton
+import com.infomaniak.mail.R
+
+fun View.setSendingButtonListener(
+    buttonState: SendingButtonState,
+    onActionBlocked: () -> Unit,
+    onActionExecute: () -> Unit
+) {
+
+    if (buttonState != SendingButtonState.Send) {
+        this.applyDisabledColor(buttonState)
+    }
+
+    this.setOnClickListener {
+        when (buttonState) {
+            SendingButtonState.Send -> onActionExecute()
+            SendingButtonState.SendingBlocked -> onActionBlocked()
+            SendingButtonState.Disabled -> Unit
+        }
+    }
+}
+
+
+private fun View.applyDisabledColor(buttonState: SendingButtonState) {
+
+    val color = ContextCompat.getColor(context, R.color.disabledIconColor)
+    when (this) {
+        is MaterialButton -> {
+            this.iconTint = ColorStateList.valueOf(color)
+            this.setTextColor(color)
+        }
+
+        is MaterialToolbar -> {
+            this.navigationIcon?.setTint(color)
+            // this.menu.forEach { item ->
+            //     item.icon?.setTint(color)
+            // }
+            this.setTitleTextColor(color)
+        }
+
+        // is ActionItemView -> {
+        //     this.setIconColor(color)
+        // }
+
+        is ImageView -> {
+            this.setColorFilter(color)
+        }
+
+        is TextView -> {
+            this.setTextColor(color)
+        }
+    }
+}
+
+
+enum class SendingButtonState {
+    Send,
+    SendingBlocked,
+    Disabled
+}
