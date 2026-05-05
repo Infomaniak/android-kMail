f4d459ba696f4871469624f58fb301dbf61888e0
Sol Rubado <sol.rubado@infomaniak.com>
2026-05-05 17:19:43 +0200

    fix: Add auto advance

--- app/src/main/java/com/infomaniak/mail/ui/MainViewModel.kt
+++ app/src/main/java/com/infomaniak/mail/ui/MainViewModel.kt
@@ -169,8 +169,6 @@ class MainViewModel @Inject constructor(
     val reportDisplayProblemTrigger = SingleLiveEvent<Unit>()
     val canInstallUpdate = MutableLiveData(false)
 
-    val autoAdvanceThreadsUids = SingleLiveEvent<List<String>>()
-
     val mailboxesLive = mailboxController.getMailboxesAsync(AccountUtils.currentUserId).asLiveData(ioCoroutineContext)
 
     //region Multi selection
@@ -885,15 +883,6 @@ class MainViewModel @Inject constructor(
 
     fun deleteThreadInRealm(threadUid: String) = viewModelScope.launch(ioCoroutineContext) {
         threadController.deleteThread(threadUid)
-    }
-
-    private suspend fun shouldAutoAdvance(message: Message?, threadsUids: List<String>): Boolean {
-        val isWorkingWithThread = message == null
-        return isWorkingWithThread || threadHasOnlyOneMessageLeft(threadsUids.first())
-    }
-
-    private suspend fun threadHasOnlyOneMessageLeft(threadUid: String): Boolean {
-        return messageController.getMessagesCountInThread(threadUid, featureFlagsLive.value, mailboxContentRealm()) == 1
     }
 
     fun shareThreadUrl(messageUid: String) {
