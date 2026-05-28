182c5c975012996b694f507772a64bbc58d9cad1
Sol Rubado <sol.rubado@infomaniak.com>
2026-05-28 15:52:56 +0200

    refactor: Make variable private

--- app/src/main/java/com/infomaniak/mail/ui/main/search/SearchViewModel.kt
+++ app/src/main/java/com/infomaniak/mail/ui/main/search/SearchViewModel.kt
@@ -95,7 +95,7 @@ class SearchViewModel @Inject constructor(
 
     private var currentUiState: SearchUiState = SearchUiState.IDLE
 
-    var currentFilters = mutableSetOf<ThreadFilter>()
+    private var currentFilters = mutableSetOf<ThreadFilter>()
     var isAllFoldersSelected: Boolean = false
 
     private var lastExecutedFolder: Folder? = null
