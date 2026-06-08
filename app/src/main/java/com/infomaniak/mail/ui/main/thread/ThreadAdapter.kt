ffa4978072631e8dc1ba3874bc23498a4525497b
Elouan BOITEUX <elouan.boiteux@infomaniak.com>
2026-06-08 18:11:01 +0200

    fix: Rename variable

--- app/src/main/java/com/infomaniak/mail/ui/main/thread/ThreadAdapter.kt
+++ app/src/main/java/com/infomaniak/mail/ui/main/thread/ThreadAdapter.kt
@@ -626,7 +626,7 @@ class ThreadAdapter(
                 isButtonEnabled = true
                 hideButtonProgress(R.string.aiButtonRetry)
 
-                if (state.asAlreadyRetried && !state.wasLoaderShown) {
+                if (state.hasAlreadyRetried && !state.wasLoaderShown) {
                     threadAdapterCallbacks?.showSnackbarRetry?.invoke(errorMessageRes)
                 }
             } else {
