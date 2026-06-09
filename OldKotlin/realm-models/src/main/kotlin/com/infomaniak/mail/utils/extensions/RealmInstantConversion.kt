5cd98a52a1a41d84307eed97a114e6d2d4cb07d5
Louis CAD <louis.cognault@infomaniak.com>
2026-06-09 16:29:43 +0200

    fix: Fix RealmInstant conversion precision
    Before, that, anything sub-second was incorrectly converted.
    It didn't have any noticeable impact because it affected only sub-second precision,
    which users rarely see.

--- OldKotlin/realm-models/src/main/kotlin/com/infomaniak/mail/utils/extensions/RealmInstantConversion.kt
+++ OldKotlin/realm-models/src/main/kotlin/com/infomaniak/mail/utils/extensions/RealmInstantConversion.kt
@@ -20,10 +20,10 @@ package com.infomaniak.mail.utils.extensions
 import io.realm.kotlin.types.RealmInstant
 import java.util.Date
 
-fun RealmInstant.toDate(): Date = Date(epochSeconds * 1_000L + nanosecondsOfSecond / 1_000L)
+fun RealmInstant.toDate(): Date = Date(epochSeconds * 1_000L + nanosecondsOfSecond / 1_000_000L)
 
 fun Date.toRealmInstant(): RealmInstant {
     val seconds = time / 1_000L
-    val nanoseconds = (time - seconds * 1_000L).toInt()
+    val nanoseconds = (time - seconds * 1_000_000L).toInt()
     return RealmInstant.from(seconds, nanoseconds)
 }
