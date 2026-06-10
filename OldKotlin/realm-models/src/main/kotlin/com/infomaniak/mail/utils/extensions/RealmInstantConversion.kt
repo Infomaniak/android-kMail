4f4fffb905f88244310ba3708c1e59bfe5471809
Louis CAD <louis.cognault@infomaniak.com>
2026-06-10 09:22:15 +0200

    fix: Fix the broken fix of Date to RealmInstant conversion

--- OldKotlin/realm-models/src/main/kotlin/com/infomaniak/mail/utils/extensions/RealmInstantConversion.kt
+++ OldKotlin/realm-models/src/main/kotlin/com/infomaniak/mail/utils/extensions/RealmInstantConversion.kt
@@ -24,6 +24,6 @@ fun RealmInstant.toDate(): Date = Date(epochSeconds * 1_000L + nanosecondsOfSeco
 
 fun Date.toRealmInstant(): RealmInstant {
     val seconds = time / 1_000L
-    val nanoseconds = (time - seconds * 1_000_000L).toInt()
+    val nanoseconds = ((time - seconds * 1_000L) * 1_000_000L).toInt()
     return RealmInstant.from(seconds, nanoseconds)
 }
