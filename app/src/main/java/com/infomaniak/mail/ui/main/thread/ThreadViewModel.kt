7459f4fedd6884e089c3d75541f090e79d261b43
Elouan BOITEUX <elouan.boiteux@infomaniak.com>
2026-06-11 11:29:51 +0200

    fix: Reset ThreadState when threadUid changes

--- app/src/main/java/com/infomaniak/mail/ui/main/thread/ThreadViewModel.kt
+++ app/src/main/java/com/infomaniak/mail/ui/main/thread/ThreadViewModel.kt
@@ -93,13 +93,16 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.distinctUntilChangedBy
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.mapLatest
 import kotlinx.coroutines.flow.mapNotNull
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.shareIn
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.invoke
@@ -240,7 +243,14 @@ class ThreadViewModel @Inject constructor(
 
     init {
         viewModelScope.launch {
-            threadFlow.filterNotNull().collect { thread ->
+            threadOpeningModeFlow
+                .mapNotNull { it.threadUid }
+                .distinctUntilChanged()
+                .onEach { _ ->
+                    threadState.reset()
+                }.launchIn(viewModelScope)
+
+            threadFlow.filterNotNull().onEach { thread ->
                 val featureFlags = featureFlagsFlow.first()
 
                 // These 2 will always be empty or not all together at the same time.
@@ -257,7 +267,7 @@ class ThreadViewModel @Inject constructor(
                     sendMatomoAboutThreadMessagesCount(thread, featureFlags)
                     if (thread.isSeen.not()) markThreadAsSeen(thread)
                 }
-            }
+            }.launchIn(viewModelScope)
         }
     }
 
