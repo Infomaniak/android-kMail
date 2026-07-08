e4bd004881e1cdd3c4fb343048746405dfe29cdf
Vincent Te <vincent.te@infomaniak.com>
2026-07-08 15:02:46 +0200

    chore: Logout user after each test to avoid too many token stored in the backend

--- app/src/androidTest/java/com/infomaniak/mail/ui/Scenarios.kt
+++ app/src/androidTest/java/com/infomaniak/mail/ui/Scenarios.kt
@@ -18,16 +18,29 @@
 package com.infomaniak.mail.ui
 
 import android.view.View
+import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.hasTestTag
+import androidx.compose.ui.test.junit4.ComposeTestRule
+import androidx.compose.ui.test.onNodeWithTag
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.UiController
 import androidx.test.espresso.ViewAction
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.isRoot
+import androidx.test.espresso.matcher.ViewMatchers.withContentDescription
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.espresso.web.sugar.Web.onWebView
 import androidx.test.espresso.web.webdriver.DriverAtoms.findElement
 import androidx.test.espresso.web.webdriver.DriverAtoms.webClick
 import androidx.test.espresso.web.webdriver.DriverAtoms.webKeys
 import androidx.test.espresso.web.webdriver.Locator
 import androidx.test.uiautomator.UiDevice
+import com.infomaniak.mail.R
+import com.infomaniak.mail.ui.Utils.onViewWithTimeout
 import org.hamcrest.Matcher
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.seconds
@@ -52,6 +65,26 @@ object Scenarios {
             .perform(webClick())
 
         onView(isRoot()).perform(waitFor(3.seconds))
+    }
+
+    @OptIn(ExperimentalTestApi::class)
+    fun logout(composeTestRule: ComposeTestRule) {
+        clickIfVisible(withContentDescription(R.string.contentDescriptionButtonBack))
+        clickIfVisible(withContentDescription(R.string.buttonClose))
+
+        onViewWithTimeout(matcher = withId(R.id.userAvatar)).perform(click())
+        onViewWithTimeout(matcher = withText(R.string.buttonAccountLogOut)).perform(click())
+        onViewWithTimeout(matcher = withText(R.string.buttonConfirm)).perform(click())
+
+        composeTestRule.waitUntilAtLeastOneExists(hasTestTag("button_next_onboarding"), 5_000)
+        composeTestRule.onNodeWithTag("button_next_onboarding").assertIsDisplayed()
+    }
+
+    private fun clickIfVisible(matcher: org.hamcrest.Matcher<View>) {
+        runCatching {
+            onView(matcher).check(matches(isDisplayed()))
+            onView(matcher).perform(click())
+        }
     }
 
     fun waitFor(delay: Duration): ViewAction = object : ViewAction {
