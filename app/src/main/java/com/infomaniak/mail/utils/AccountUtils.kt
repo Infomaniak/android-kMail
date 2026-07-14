63187e4de10bd081adb3708126639b9bf96f4f74
Elouan BOITEUX <elouan.boiteux@infomaniak.com>
2026-07-14 14:04:27 +0200

    feat: Keep local value for card

--- app/src/main/java/com/infomaniak/mail/utils/AccountUtils.kt
+++ app/src/main/java/com/infomaniak/mail/utils/AccountUtils.kt
@@ -128,7 +128,8 @@ object AccountUtils : CredentialManager() {
             if (remoteUser.id == currentUserId) {
                 remoteUser.organizations = arrayListOf()
                 requestCurrentUser()?.let { localUser ->
-                    setUserToken(remoteUser, localUser.apiToken)
+                    val remoteUserWithLocalCard = remoteUser.copy(card = localUser.card)
+                    setUserToken(remoteUserWithLocalCard, localUser.apiToken)
                 }
             }
         }
