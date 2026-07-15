f55a412322dfc5ed76b6995e686df1016a6b7f40
Sol Rubado <sol.rubado@infomaniak.com>
2026-07-15 17:11:40 +0200

    refactor: Make mentions common code script more readable

--- app/src/main/res/raw/mentions_common_code.js
+++ app/src/main/res/raw/mentions_common_code.js
@@ -1,5 +1,7 @@
+const MENTION_ANCHOR_SELECTOR = "a[data-ik-mention-ref]";
+
 // We get the parent block to get the correct range to look for the @
-function getBlockParent(node) {
+const getBlockParent = (node) => {
     const editor = getEditor();
     let current = node;
 
@@ -12,8 +14,39 @@ function getBlockParent(node) {
     return editor;
 };
 
-function getTextBeforeCaret(stripMentions = true) {
-    const anchorMention = "a[data-ik-mention-ref]"
+const defineSelectionRange = (block, range, editor, stripMentions) => {
+    const selectionRange = range.cloneRange();
+
+    // Set start of range to the start of the parent block if there is, and if not to the start of the editor
+    if (block?.nodeType === Node.ELEMENT_NODE) {
+        selectionRange.setStart(block, 0);
+    } else {
+        selectionRange.selectNodeContents(editor);
+    }
+
+    // Get the last element of the range, if it is a text context get the parent element
+    const endElement = range.endContainer.nodeType === Node.ELEMENT_NODE
+            ? range.endContainer
+            : range.endContainer.parentElement;
+
+    // Find if the closest element to endElement that is a mentionAnchor, returns itself if the endElement it's a mention anchor
+    const activeMention = endElement?.closest?.(MENTION_ANCHOR_SELECTOR);
+
+    // In the case we want to strip mentions and there is an active mention, we make sure the mention is included in the range.
+    if (stripMentions && activeMention && block.contains(activeMention)) {
+        selectionRange.setEndAfter(activeMention);
+    } else {
+        selectionRange.setEnd(range.endContainer, range.endOffset);
+    }
+
+    return selectionRange;
+}
+
+const stripMentionsFromSelection = (selection) => {
+    selection.querySelectorAll(MENTION_ANCHOR_SELECTOR).forEach(mention => mention.replaceWith(" "));
+}
+
+function getTextBeforeCaret (stripMentions = true) {
     const selection = globalThis.getSelection();
     if (!selection || selection.rangeCount === 0) return "";
 
@@ -24,30 +57,13 @@ function getTextBeforeCaret(stripMentions = true) {
     if (!editor.contains(range.startContainer)) return "";
 
     const block = getBlockParent(range.startContainer);
-    const preRange = range.cloneRange();
+    const selectionRange = defineSelectionRange(block, range, editor, stripMentions);
+    const clonedSelection = selectionRange.cloneContents();
 
-    if (block?.nodeType === Node.ELEMENT_NODE) {
-        preRange.setStart(block, 0);
-    } else {
-        preRange.selectNodeContents(editor);
-    }
-
-    const endElement = range.endContainer.nodeType === Node.ELEMENT_NODE
-            ? range.endContainer
-            : range.endContainer.parentElement;
-    const activeMention = endElement?.closest?.(anchorMention);
-
-    if (stripMentions && activeMention && block.contains(activeMention)) {
-        preRange.setEndAfter(activeMention); // include full mention, then replace it with space
-    } else {
-        preRange.setEnd(range.endContainer, range.endOffset);
-    }
-
-    const fragment = preRange.cloneContents();
     if (stripMentions) {
         // Replace all the existing mentions with a space to ignore them when extracting the query
-        fragment.querySelectorAll(anchorMention).forEach((mention) => mention.replaceWith(" "));
+        stripMentionsFromSelection(clonedSelection);
     }
 
-    return fragment.textContent;
+    return clonedSelection.textContent;
 }
