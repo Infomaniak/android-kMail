842efaae9a9145138a6c12ddf47077887678321c
Elouan BOITEUX <elouan.boiteux@infomaniak.com>
2026-07-30 16:30:55 +0200

    feat: Improve bindReminder by removing divider and displaying iconReminder

--- app/src/main/java/com/infomaniak/mail/ui/main/thread/ThreadAdapter.kt
+++ app/src/main/java/com/infomaniak/mail/ui/main/thread/ThreadAdapter.kt
@@ -885,6 +885,7 @@ class ThreadAdapter(
         reminderAlert.isGone = true
         endReminderAlert.isGone = true
         requestResponseAlert.isGone = true
+        iconReminder.isVisible = message.reminder != null && threadAdapterState.isExpandedMap[message.uid] != true
 
         if (featureFlags?.contains(FeatureFlag.RESPONSE_REQUIRED) != true) return
 
@@ -901,6 +902,7 @@ class ThreadAdapter(
                 message.allRecipients.any { it.isMe() } -> bindRecipientReminderAlert(message, reminderDate, isExpired)
             }
         }
+        hideAlertGroupIfNoneDisplayed()
     }
 
     private fun ItemMessageBinding.bindScheduledDraftReminderAlert(message: Message) {
