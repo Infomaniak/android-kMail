2707df50f56f2e571f09b921cf0023f855a54be2
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2026-08-26 15:43:08 +0200

    feat: Replace old code reference to mail's R

--- app/src/main/java/com/infomaniak/mail/ui/alertDialogs/TitleAlertDialog.kt
+++ app/src/main/java/com/infomaniak/mail/ui/alertDialogs/TitleAlertDialog.kt
@@ -1,6 +1,6 @@
 /*
  * Infomaniak Mail - Android
- * Copyright (C) 2023-2024 Infomaniak Network SA
+ * Copyright (C) 2023-2026 Infomaniak Network SA
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -19,7 +19,7 @@ package com.infomaniak.mail.ui.alertDialogs
 
 import android.content.Context
 import androidx.annotation.StringRes
-import com.infomaniak.mail.R
+import com.infomaniak.core.common.R
 import dagger.hilt.android.qualifiers.ActivityContext
 import dagger.hilt.android.scopes.ActivityScoped
 import javax.inject.Inject
