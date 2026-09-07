6c7246e0e25d1c8b64d5da9f1beb00b9688d5d2c
Jamy Bailly <jamy.bailly@infomaniak.com>
2026-09-07 16:49:12 +0200

    chore: Use snapshot old kotlin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,10 +18,10 @@ plugins {
 
 val enableLeakCanary = false
 
-val appCompileSdk: Int by rootProject.extra
-val appTargetSdk: Int by rootProject.extra
-val appMinSdk: Int by rootProject.extra
-val javaVersion: JavaVersion by rootProject.extra
+val appCompileSdk: Int = rootProject.extra["appCompileSdk"] as Int
+val appTargetSdk: Int = rootProject.extra["appTargetSdk"] as Int
+val appMinSdk: Int = rootProject.extra["appMinSdk"] as Int
+val javaVersion: JavaVersion = rootProject.extra["javaVersion"] as JavaVersion
 
 android {
     namespace = "com.infomaniak.mail"
