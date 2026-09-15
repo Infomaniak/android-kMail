09e18fb3534c8dbfc8d23a180966319ad0e652cc
Abdourahamane Boinaidi <abdourahamane.boinaidi@infomaniak.com>
2026-09-15 10:19:15 +0200

    fix: Disallow base tag in HeadCleaner
    The <base> tag is not currently supported by our webmail, and other email clients also tend to strip it.
    
    Supporting it properly would require additional work, so exclude it from the allowed head tags for now.

--- HtmlCleaner/src/main/java/com/infomaniak/html/cleaner/HeadCleaner.kt
+++ HtmlCleaner/src/main/java/com/infomaniak/html/cleaner/HeadCleaner.kt
@@ -30,7 +30,7 @@ import org.jsoup.parser.Tag
 import org.jsoup.select.NodeTraversor
 import org.jsoup.select.NodeVisitor
 
-private val ALLOWED_TAGS = listOf("style", "meta", "base")
+private val ALLOWED_TAGS = listOf("style", "meta")
 
 internal class HeadCleaner {
     fun clean(dirtyDocument: Document, cleanedDocument: Document) {
